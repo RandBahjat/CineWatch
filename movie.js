e47cb8888eece9705327685e45d8bc4f365ec55a
RandiBajat <randibajat@github.com>
2026-08-10 23:01:18 +0300

    Auto-sync: 8/10/2026, 11:01:18 PM

--- movie.js
+++ movie.js
@@ -8598,7 +8598,7 @@ const MOVIES = [
           { episode: 2, title: "Tread Lightly" },
           { episode: 3, title: "Double Cross" },
           { episode: 4, title: "A Mistake I'll Never Make Again" },
-          { episode: 5, title: "The Wolf and the Lion" },
+          { episode: 5, title: "Betrayal" },
           { episode: 6, title: "A Golden Crown" },
           { episode: 7, title: "You Win or You Die" },
           { episode: 8, title: "The Pointy End" },
