6361d4932abefe33d063d244866fece2b437421a
RandiBajat <randibajat@github.com>
2026-08-13 20:10:58 +0300

    fix: syntax error in server.js

--- server.js
+++ server.js
@@ -226,5 +226,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(\`CineWatch custom backend running on port \${PORT}\`);
+  console.log(`CineWatch custom backend running on port ${PORT}`);
 });
