d026096be0d3ce1833d5e11b128afb123dc4556d
RandiBajat <randibajat@github.com>
2026-08-15 21:53:35 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -3600,7 +3600,7 @@ const MOVIES = [
     age: "R",
     duration: "1h 48m",
     genres: ["Action", "Sci-Fi", "Thriller"],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/rFhKkXhk7ClU03jQ5rHIApJDwev.jpg",
+    poster: "https://image.tmdb.org/t/p/original/niPqU1GGEqLoUUa8QnXNsbgGW0D.jpg",
     backdrop: "https://framerusercontent.com/images/4pNHQEiyCdfmb6ye9u6EFl31UI.webp?width=1000&height=667",
     videoUrl: "1265609",
     overview: "During the final 24 hours of an ultra-tough selection process, an elite team of Army Rangers encounters an unimaginable, deadly threat from beyond this world while deep on a training mission.",
