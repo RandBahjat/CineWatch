d037f5b14892353e9ed0f832126c7e6c4c502634
RandiBajat <randibajat@github.com>
2026-08-15 21:06:42 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10247,7 +10247,7 @@ const MOVIES = [
     age: "R",
     duration: "1h 27m",
     genres: ["Action", "Crime", "Thriller"],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/dncJ81z1BahrT3ogLvlxOUC5n4u.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/cdm17vK8PxHfTi7ayZf6WKbOgUO.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/AvIiUxbIrxwbLAhLgMGIpCEeneP.jpg",
     videoUrl: "4108",
     overview: "A high-octane action thriller following Frank Martin, a quiet, elite mercenary living in the south of France who delivers package deliveries with zero questions asked. Guided by three strict rules—never change the deal, no names, and never look in the package—Frank's life shifts into overdrive when he breaks his own rules and discovers his latest cargo is a bound woman linked to an international human trafficking ring.",
