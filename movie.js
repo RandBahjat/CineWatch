857901d75d3696a7efa7b894595d9ab39466eb92
RandiBajat <randibajat@github.com>
2026-08-15 19:03:17 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10036,6 +10036,23 @@ const MOVIES = [
     featured: false,
   },
   {
+    title: "Regretting You",
+    type: "Movie", // Use "Movie" or "TV Show"
+    year: 2025,
+    rating: 7.0,
+    age: "PG-13",
+    duration: "1h 32m",
+    genres: ["Comedy","Drama","Romance"],
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/v5f1qO6NJnxWgONgkYg21TW39DT.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/30rvmoEBqovm723agDJ3QddWXcY.jpg",
+    videoUrl: "37735",
+    overview: "A clean-cut high schooler's life takes an unexpected turn after a little white lie about losing her virginity spreads through the school's rumor mill. As her reputation spirals wildly out of control, she decides to embrace her new wild child image, cleverly turning gossip into personal gain — all while inadvertently learning what it really means to stand up for herself.",
+    director: "Will Gluck",
+    cast: ["Emma Stone, Amanda Bynes, Penn Badgley, Patricia Clarkson, Thomas Haden Church, Stanley Tucci, Lisa Kudrow"],
+    trending: false, // Leave false so it only shows up in the Genres tab
+    featured: false,
+  },
+  {
     title: "Spider Noir",
     type: "TV Show",
     year: 2026,
