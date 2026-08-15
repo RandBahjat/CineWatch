e5497d39e1b711b1e049694b27eb9354d9fcfbb9
RandiBajat <randibajat@github.com>
2026-08-15 21:31:49 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10349,7 +10349,7 @@ const MOVIES = [
     age: "PG-13",
     duration: "1h 42m",
     genres: ["Action","Comedy", "Crime"],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/qrt65VVaP5DEEBZgB5XYctA82Qv.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/qKvVUHdiwqkXdlRsW4fHO7SAZef.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/w1M7LydgxFs3gLgYxxNtDODMrYE.jpg",
     videoUrl: "168530",
     overview: "Ben Barber, a fast-talking high school security guard, aspires to prove himself worthy of marrying Angela, the love of his life. To gain the approval of her tough, veteran LAPD detective brother, James, Ben agrees to a 24-hour shift on the streets of Atlanta. What starts as a series of staged harmless calls escalates into a real-world confrontation with one of the city's most dangerous crime lords.",
