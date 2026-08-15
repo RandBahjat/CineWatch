9fcf10d8861a3a9c906989878f2787eda4a5bd6b
RandiBajat <randibajat@github.com>
2026-08-15 21:46:21 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10454,7 +10454,7 @@ const MOVIES = [
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/7gKI9hpEMcZUQpNgKrkDzJpbnNS.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/3oKSoTLgJFsBUXe0amkKoSlKYNo.jpg",
     videoUrl: "697843",
-    overview: "Tyler Rake, a fearless black-market mercenary with nothing left to lose, is hired to rescue the kidnapped son of an imprisoned international crime lord. Dispatched to Dhaka, Bangladesh, Rake executes a daring extraction, but the mission devolves into a deadly fight for survival when local authorities and rival cartels lock down the city to trap them.",
+    overview: "After barely surviving his grievous wounds from his mission in Dhaka, black-ops mercenary Tyler Rake retires to a remote cabin in Austria. However, his peace is short-lived when he is recruited for another high-stakes extraction: rescuing the battered family of a ruthless Georgian gangster from a high-security prison. When the break-out turns deadly, the gangster's vengeful brother pursues Rake and the family across Vienna.",
     director: "Sam Hargrave",
     cast: ["Chris Hemsworth, Rudhraksh Jaiswal, Randeep Hooda, Golshifteh Farahani, Pankaj Tripathi, David Harbour"],
     trending: false, // Leave false so it only shows up in the Genres tab
