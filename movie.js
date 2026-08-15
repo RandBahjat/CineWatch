cebb293c7c177901534a2d7c45fe8c3f634e95cd
RandiBajat <randibajat@github.com>
2026-08-15 18:37:55 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -9928,7 +9928,7 @@ const MOVIES = [
     backdrop: "https://image.tmdb.org/t/p/original/ifnnW3q4rcxGiqFQf2CLG8Ms4p9.jpg",
     videoUrl: "1035259",
     overview: "Only one man has the particular set of skills to lead Police Squad and save the world — Lt. Frank Drebin Jr. Following in his father's footsteps, he must solve a murder case to prevent Police Squad from being shut down for good, in this reboot of the beloved spoof comedy franchise.",
-    director: "Will Gluck",
+    director: "Akiva Schaffer",
     cast: ["Sydney Sweeney, Glen Powell, Alexandra Shipp, GaTa, Michelle Hurd, Bryan Brown, Rachel Griffiths, Dermot Mulroney"],
     trending: false, // Leave false so it only shows up in the Genres tab
     featured: false,
