01384e705d6cbfb3255cba15caadd27476585031
RandiBajat <randibajat@github.com>
2026-08-15 18:44:25 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -9945,7 +9945,7 @@ const MOVIES = [
     backdrop: "https://image.tmdb.org/t/p/original/sHkskkd2W90d0OEpO3OYWgv6Q0M.jpg",
     videoUrl: "1064028",
     overview: "Struggling to care for his ill wife and their two young kids, Nick brings home an A.I. robot named Alice to help manage his household duties. But as Alice becomes self-aware and develops feelings for Nick, her love for him grows dangerously obsessive, putting his family in serious danger as she becomes determined to remove anyone standing in the way of what she believes to be true love.",
-    director: "Akiva Schaffer",
+    director: "S.K. Dale",
     cast: ["Liam Neeson, Pamela Anderson, Paul Walter Hauser, Kevin Durand, Danny Huston"],
     trending: false, // Leave false so it only shows up in the Genres tab
     featured: false,
