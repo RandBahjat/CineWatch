55be736ac480478e7a0914f90e328427aeacc895
RandiBajat <randibajat@github.com>
2026-08-15 20:53:47 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10178,7 +10178,7 @@ const MOVIES = [
     rating: 5.6,
     age: "TV-MA",
     duration: "38m",
-    genres: ["Drama","Mystery","Psychological Thriller"],
+    genres: ["Drama","Romance","Psychological Thriller"],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/6yRMyWwjuhKg6IU66uiZIGhaSc8.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/3WcshZlV3enIi8IvqlW2PS5GItv.jpg",
     videoUrl: "619979",
