976c3bdfe50620758676a21ad98031e7874e6dd1
RandiBajat <randibajat@github.com>
2026-08-15 21:43:13 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10421,8 +10421,8 @@ const MOVIES = [
     backdrop: "https://image.tmdb.org/t/p/original/pLOFQLeKQYsCzkYgsw9tg1cPuDG.jpg",
     videoUrl: "725201",
     overview: "When Court Gentry—a top-tier CIA mercenary operating under the codename Sierra Six—uncovers dark agency secrets during a mission, he becomes the prime target of his rogue superiors. To eliminate him, the CIA unleashes Lloyd Hansen, a sociopathic former agent turned mercenary who launches a relentless, global manhunt across Europe to hunt Six down.",
-    director: "Dexter Fletcher",
-    cast: ["Chris Evans, Ana de Armas, Adrien Brody, Mike Moh, Amy Sedaris, Tate Donovan, Tim Blake Nelson"],
+    director: "Anthony Russo, Joe Russo",
+    cast: ["Ryan Gosling, Chris Evans, Ana de Armas, Jessica Henwick, Regé-Jean Page, Wagner Moura, Julia Butters, Dhanush, Alfre Woodard, Billy Bob Thornton"],
     trending: false, // Leave false so it only shows up in the Genres tab
     featured: false,
   },
