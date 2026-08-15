cc28e4e910159dc71aa6b16d7a246250ba87f296
RandiBajat <randibajat@github.com>
2026-08-15 18:59:15 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10024,7 +10024,7 @@ const MOVIES = [
     year: 2010,
     rating: 7.0,
     age: "PG-13",
-    duration: "1h 48m",
+    duration: "1h 32m",
     genres: ["Comedy","Romance"],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/9C09L1U9S13J4Ut029Qhu0oLcTQ.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/rSEfKLHDZKiMQWsyJIfKr9D0CaC.jpg",
