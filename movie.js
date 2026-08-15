a3904b7e3fff5f3b8b671333ba23b14a9de9a94e
RandiBajat <randibajat@github.com>
2026-08-15 21:01:26 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10214,7 +10214,7 @@ const MOVIES = [
     duration: "1h 45m",
     genres: ["Drama", "Romance", "Thriller"],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/7IGKrY1f1KfwMipx9wZC4NRgIdF.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/iuyE1qy9HoTsMg0DHB1KJxM94DJ.jpg",
     videoUrl: "216015",
     overview: "A German erotic short drama following Antonia, a overwhelmed single mother working as a maid in a luxury Berlin hotel. After sending her young son off to visit his father, she rushes to work overwhelmed by routine and isolation. While cleaning an apparently empty guest suite, she unexpectedly encounters Julius, a blind painter. A silent, sensual connection rapidly builds between them, sparking a transformative, passionate encounter that reawakens her long-repressed intimacy and desires.",
     director: "Sergej Moya",
