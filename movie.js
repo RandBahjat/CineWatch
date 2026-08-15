6bdc727ad905ca8e745b40a26be71a3d13893943
RandiBajat <randibajat@github.com>
2026-08-15 21:09:00 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10265,7 +10265,7 @@ const MOVIES = [
     duration: "1h 44m",
     genres: ["Action", "Crime", "Thriller"],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/o4w9VF3GRZuXe9NHHvZ07pJespl.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/ghgogVFRi60o0sVsRWgpG2r4Sy9.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/nNbxrSyKUTMVwpnFgnKH9ZV0QZ8.jpg",
     videoUrl: "9335",
     overview: "Ex-special forces operative Frank Martin has relocated to Miami, taking a low-profile temporary job driving and protecting the young son of a high-ranking US anti-drug official. When the boy is kidnapped and infected with a deadly, highly contagious airborne virus, Frank is framed for the crime and must go on the run to stop a ruthless cartel plot before time runs out.",
     director: "Louis Leterrier",
