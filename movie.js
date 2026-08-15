e3bd7663591b1b8e45267b161d5348dbcff2c26a
RandiBajat <randibajat@github.com>
2026-08-15 22:06:20 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10580,6 +10580,23 @@ const MOVIES = [
     featured: false,
   },
   {
+    title: "Batman Returns",
+    type: "Movie", // Use "Movie" or "TV Show"
+    year: 1992,
+    rating: 7.1,
+    age: "PG-13",
+    duration: "2h 6m",
+    genres: ["Action","Crime", "Fantasy","Thriller"],
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/jKBjeXM7iBBV9UkUcOXx3m7FSHY.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/zlsaQEE26TS34ziXAiNIAqa0MLX.jpg",
+    videoUrl: "364",
+    overview: "As Gotham City prepares for Christmastime, the bizarre and vengeful Penguin emerges from the sewers, teaming up with corrupt tycoon Max Shreck to run for mayor and frame the Caped Crusader. Meanwhile, shy secretary Selina Kyle transforms into the seductive and lethal Catwoman, launching her own dangerous campaign against Shreck and Batman. Surrounded by new foes and conflicting loyalties, Batman must defend Gotham against total chaos.",
+    director: "Tim Burton",
+    cast: ["Michael Keaton, Danny DeVito, Michelle Pfeiffer, Christopher Walken, Michael Gough, Pat Hingle"],
+    trending: false, // Leave false so it only shows up in the Genres tab
+    featured: false,
+  },
+  {
     title: "Spider Noir",
     type: "TV Show",
     year: 2026,
