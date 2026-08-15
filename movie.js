d9bd768657f882d81bc9f9a8d4363238d642915c
RandiBajat <randibajat@github.com>
2026-08-15 22:07:51 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10573,7 +10573,7 @@ const MOVIES = [
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/cij4dd21v2Rk2YtUQbV5kW69WB2.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/qMsbXoFEzqsCZxA8i4szTPLw64d.jpg",
     videoUrl: "268",
-    overview: "As Gotham City prepares for Christmastime, the bizarre and vengeful Penguin emerges from the sewers, teaming up with corrupt tycoon Max Shreck to run for mayor and frame the Caped Crusader. Meanwhile, shy secretary Selina Kyle transforms into the seductive and lethal Catwoman, launching her own dangerous campaign against Shreck and Batman. Surrounded by new foes and conflicting loyalties, Batman must defend Gotham against total chaos.",
+    overview: "In crime-ridden Gotham City, billionaire Bruce Wayne operates in secret as the enigmatic vigilante Batman, keeping violent criminals at bay. When mob enforcer Jack Napier is disfigured during a shootout at Axis Chemicals, he transforms into the sadistic, flamboyant Joker and launches a reign of terror over the city. As photojournalist Vicki Vale unravels Bruce's true identity, Batman faces off against the Joker in a dark, decisive battle for Gotham's survival.",
     director: "Tim Burton",
     cast: ["Michael Keaton, Danny DeVito, Michelle Pfeiffer, Christopher Walken, Michael Gough, Pat Hingle"],
     trending: false, // Leave false so it only shows up in the Genres tab
