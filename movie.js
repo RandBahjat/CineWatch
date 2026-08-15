20a3678182bcdd6e77390a0277e81d971e96bf9f
RandiBajat <randibajat@github.com>
2026-08-15 19:08:51 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -10062,7 +10062,7 @@ const MOVIES = [
     genres: ["Drama","Romance","Musical"],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/4JyNWkryifWbWXJyxcWh3pVya6N.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/iQlJyRecJeGGzQGT2rEcyAgz89F.jpg",
-    videoUrl: "1327862",
+    videoUrl: "762975",
     overview: "Cassie, a struggling singer-songwriter, and Luke, a Marine about to be deployed, enter into a marriage of convenience for the practical benefits — health insurance for her, extra pay for him. But as their fake relationship deepens through letters and unexpected visits, their feelings for each other become dangerously real, blurring the line between the arrangement they made and the love they didn't plan for.",
     director: "Elizabeth Allen Rosenbaum",
     cast: ["Sofia Carson, Nicholas Galitzine, John Harlan Kim, Chosen Jacobs, Linden Ashby, Nicholas Logan"],
