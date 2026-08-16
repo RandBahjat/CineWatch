e020e48322de0ec17e00e3f772cc04ef288c73c9
RandiBajat <randibajat@github.com>
2026-08-16 20:18:55 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -11205,7 +11205,7 @@ const MOVIES = [
     year: 2012,
     rating: 8.5,
     age: "R",
-    duration: "2h 41m",
+    duration: "2h 45m",
     genres: ["Action","Comedy","Crime", "Thriller"],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/lbBWwxBht4JFP5PsuJ5onpMqugW.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/zpEWFNqoN8Qg1SzMMHmaGyOBTdW.jpg",
