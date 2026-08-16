2fb0b27efbf6b005f0f7b69cf3fed080d687d62c
RandiBajat <randibajat@github.com>
2026-08-16 20:33:59 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -11297,7 +11297,7 @@ const MOVIES = [
     videoUrl: "1011477",
     overview: "Martial arts prodigy Li Fong leaves Beijing to move to New York City with his mother, struggling to adapt to a new life while navigating local martial arts conflicts. Guided by his wise mentor Mr. Han and legendary Sensei Daniel LaRusso, Li Fong must unite two distinct martial arts philosophies to face a formidable opponent on the ultimate stage.",
     director: "Jonathan Entwistle",
-    cast: ["Clive Owen, Morgan Freeman, Cliff Curtis, Aksel Hennie, Shohreh Aghdashloo, Peyman Maadi, Ayelet Zurer"],
+    cast: ["Jackie Chan, Ralph Macchio, Ben Wang, Joshua Jackson, Sadie Stanley, Ming-Na Wen"],
     trending: false, // Leave false so it only shows up in the Genres tab
     featured: false,
   },
