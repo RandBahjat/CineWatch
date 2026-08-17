a339cd4ec0f68782700ff194cd38dae2d13e5583
RandiBajat <randibajat@github.com>
2026-08-17 15:59:44 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -257,8 +257,8 @@ const MOVIES = [
     videoUrl:
       "1493400",
     overview:
-      "Veronica Rodriguez",
-    director: "Travis Knight",
+      "Connect 3 lose opening act for reunion tour, head back to Camp Rock to find next big thing. Campers vie for spot, sparks rivalries, romances, alliances.",
+    director: "Veronica Rodriguez",
     cast: ["Nicholas Galitzine, Camila Mendes, Idris Elba, Jared Leto, Alison Brie, Morena Baccarin, James Purefoy"],
     trending: true,
     featured: false,
