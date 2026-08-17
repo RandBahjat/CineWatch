211958da0660c63023d5ab6eb1dd052f40244e48
RandiBajat <randibajat@github.com>
2026-08-17 14:07:12 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -65,7 +65,7 @@ const MOVIES = [
     poster:
       "https://www.themoviedb.org/t/p/w600_and_h900_face/iPOn6DinuVyLY17YM9mKuPofV08.jpg",
     backdrop:
-      "https://image.tmdb.org/t/p/original/nfNzgUZ4M8XPSIFWNNXnmOjACxU.jpg",
+      "https://image.tmdb.org/t/p/original/dzeCorl61ZdxbzfnciUkrc8JSZj.jpg",
     videoUrl:
       "969681",
     overview:
