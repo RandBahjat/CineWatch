47a508e69d71f719c09d3a74b0aa60f1a0686696
RandiBajat <randibajat@github.com>
2026-08-19 15:58:12 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -47828,7 +47828,7 @@ window._SERIES_DATA = [
         season: 3,
         episodes: [
           { episode: 1, title: 'Many Happy Returns' },
-          { episode: 2, title: 'Episode 2' },
+          { episode: 2, title: 'The Empty Hearse' },
           { episode: 3, title: 'Episode 3' },
           { episode: 4, title: 'Episode 4' }
         ]
