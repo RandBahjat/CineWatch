efd5d0c9817f5e98676b5e6c1b9960cdf79ace54
RandiBajat <randibajat@github.com>
2026-08-19 14:06:05 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -42,7 +42,7 @@ window._MOVIES_DATA = [
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/iPOn6DinuVyLY17YM9mKuPofV08.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/qeQJx07rK2xm8SD2sJxFKhE7gs0.jpg",
     videoUrl: "969681",
-    overview: "Joe and Angela's marriage is on thin ice. When they invite their enigmatic upstairs neighbors for a dinner party, the night spirals into unexpected places — testing the fragile boundaries between their relationship, their neighbors, and everything they thought they wanted.",
+    overview: "Peter Parker fight crime full-time as Spider-Man in world that no longer remember him, pressure of watching old friend move on without him spark change he not sure he can control. Transformation might be only thing that can stop shocking new threat to city, and those he love — powerful villain no one can even see",
     director: "Olivia Wilde",
     cast: [
       "Seth Rogen, Olivia Wilde, Penélope Cruz, Edward Norton"
