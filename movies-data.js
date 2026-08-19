a927af0d673df2af31152cb15e9cf12069fa3ee3
RandiBajat <randibajat@github.com>
2026-08-19 15:19:44 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -8358,7 +8358,7 @@ window._MOVIES_DATA = [
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/chpWmskl3aKm1aTZqUHRCtviwPy.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/pUNfHmVqfwRdILhCkU8TdysVOXo.jpg",
-    videoUrl: "533",
+    videoUrl: "533533",
     overview: "A highly sophisticated program, Ares, is sent from the digital world into the real world on a dangerous mission — marking humankind's first encounter with A.I. beings. The film explores artificial intelligence originating from the Grid crossing into reality, with Kevin Flynn returning to confront the consequences of a world not yet ready for contact.",
     director: "Joachim Rønning ",
     cast: [
