a0d466302a2305edf3a7bfa43c525f6e999e24d2
RandiBajat <randibajat@github.com>
2026-08-19 18:17:57 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -47850,8 +47850,8 @@ window._SERIES_DATA = [
     duration: "22m",
     genres: [
       "Animation",
-      "Action & Adventure",
-      "Sci-Fi & Fantasy",
+      "Action  Adventure",
+      "Sci-Fi Fantasy",
       "Kids"
     ],
     poster: "https://image.tmdb.org/t/p/original/p276PkRJ5Wexq91DA5RkbxTYMDn.jpg",
