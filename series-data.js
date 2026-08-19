fbf3e6c4b9136a9ba6c0f70828ccccc0748abb27
RandiBajat <randibajat@github.com>
2026-08-19 15:56:52 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -47811,7 +47811,7 @@ window._SERIES_DATA = [
         season: 1,
         episodes: [
           { episode: 1, title: 'Unaired Pilot' },
-          { episode: 2, title: 'Episode 2' },
+          { episode: 2, title: 'A Study in Pink' },
           { episode: 3, title: 'Episode 3' }
         ]
       },
