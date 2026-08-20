62ff91633668ea8a0d4443c7419a3e335f703dbd
RandiBajat <randibajat@github.com>
2026-08-20 23:26:04 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -19908,8 +19908,7 @@ window._SERIES_DATA = [
           },
           {
             episode: 4,
-            title: 'You Who Cannot See, Think of Those Who Can
-'
+            title: 'You Who Cannot See, Think of Those Who Can'
           },
           {
             episode: 5,
