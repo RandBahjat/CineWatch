1e12fd4a2e6265484a128a2d21d525676594f984
RandiBajat <randibajat@github.com>
2026-08-20 14:25:20 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -2115,7 +2115,7 @@ window._MOVIES_DATA = [
       "Sci-Fi"
     ],
     poster: "https://image.tmdb.org/t/p/original/AsDFmGZ4BS57xKi94XND8l32NFt.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/16M8zTkadi0nslWjqpzCPzsYjuq.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/69EFgWWPFWbRNHmQgYdSnyJ94Ge.jpg",
     videoUrl: "49521",
     overview: "Sent to Earth as an infant to escape the destruction of Krypton, Clark Kent grows up grappling with immense powers he doesn't fully understand and a life spent hiding who he truly is. When the ruthless General Zod arrives seeking to reshape Earth into a new Krypton, Clark must finally step into his destiny — embracing the legacy of his Kryptonian heritage and the values instilled by his human parents to become the hero the world needs.",
     director: "Zack Snyder",
