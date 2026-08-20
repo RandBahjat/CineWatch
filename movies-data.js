d5aef4d8b65adb112e72f9b2d415ee9cb6579e9c
RandiBajat <randibajat@github.com>
2026-08-20 17:15:19 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10409,7 +10409,7 @@ window._MOVIES_DATA = [
       "Animation",
       "Sci-Fi"
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/nuGIlNAJUati4qEAH1nrYKUA3xa.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/mrphHnLo1ywe7yGcA5WhYlx9v3n.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/8bUSgDE11dtcQmHLvGGhAQOWMlE.jpg",
     videoUrl: "408647",
     overview: "New member Terra join Teen Titans, but hide dark secret, secretly work for villain Deathstroke, aim betray team from inside. Titans face deception, must find way stop Deathstroke's plan before too late.",
