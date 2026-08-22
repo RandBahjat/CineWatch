b3389b2a041b172d72a60a3a30fb96377dc0a61d
RandiBajat <randibajat@github.com>
2026-08-22 20:24:13 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -27,7 +27,7 @@
 const FEATURED_TITLES = ["Reacher","Lanterns", "House of the Dragon", "The Invite", "Spider-Man: Brand New Day", "The Odyssey", "Obsession", "The Last House","Silo"];
 const TRENDING_THIS_WEEK_MOVIES = ["Spider-Man: Brand New Day","The Odyssey","Obsession","Minions & Monsters","The Last House","Disclosure Day","The Invete","The End of Oak Street","Backrooms","Camp Rock 3","Evil Dead Burn","Project Hail","Supergirl"]; // Add titles here for 'Trending Movies This Week'
 const TRENDING_THIS_WEEK_SERIES = ["Lanterns","Reacher","Lucky","Silo","Ted Lasso","X-Men '97"]; // Add titles here for 'Trending Series This Week'
-const POPULAR_MOVIES = ["Spider-Man: Brand New Day", "The Odyssey", "Minions & Monsters", "The Invite", "Spider-Man: No Way Home", "Disclosure Day", "The Last House", "Michael", "Project Hail Mary"]; // Add titles here for 'Popular Movie'
+const POPULAR_MOVIES = ["Spider-Man: Brand New Day", "The Odyssey", "Minions & Monsters", "The Invite", "Spider-Man: No Way Home","The End of Oak Street", "Disclosure Day", "The Last House", "Michael", "Project Hail Mary"]; // Add titles here for 'Popular Movie'
 const POPULAR_SERIES = ["Reacher", "Ted Lasso"]; // Add titles here for 'Popular Series'
 
 // ==========================================
