f780f37d69afed793e2c86075dd828bb6d8a0972
RandiBajat <randibajat@github.com>
2026-08-22 20:34:26 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -52861,7 +52861,7 @@ window._SERIES_DATA = [
     videoUrl: "299167",
     overview: "Beth Dutton, Rip Wheeler gamble everything on new life in South Texas, but promise of building future far from ghost of Yellowstone quickly collide with brutal new reality, rival ranch that stop at nothing protect its empire.",
     director: "Chad Feehan",
-    cast: ["Kelly Reilly, Cole Hauser, Finn Little, Ed Harris, Annette Bening"],
+    cast: ["Kelly Reilly, Cole Hauser, Ed Harris, Annette Bening, Finn Little, Jai Courtney"],
     trending: false,
     featured: false,
     is4k: true,
