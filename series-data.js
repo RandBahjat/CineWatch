38f0d55611b0625e8326a1d89dc1a4bc0965040a
RandiBajat <randibajat@github.com>
2026-08-22 19:41:56 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -11,7 +11,7 @@ window._SERIES_DATA = [
     age: "TV-14",
     genres: ["Animation", "Action", "Sci-Fi"],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/2HKBc5UiFw8JrruHq8S1Y7TnlW0.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/rYc1eK8C843w8670IIfQvF01jWk.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/iDnTAeR2WNA62XQG0ivtteDSjd5.jpg",
     videoUrl: "114472",
     overview: "X-Men '97 is a revival of the classic animated series, following the iconic mutant team as they navigate high-stakes adventures and emotional challenges in a world that hates and fears them.",
     director: "Jake Castorena",
