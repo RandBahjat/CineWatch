43af4f0923e8f2e4dcdab4f2567ee571ab127972
RandiBajat <randibajat@github.com>
2026-08-22 20:33:19 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -52857,7 +52857,7 @@ window._SERIES_DATA = [
     age: "TV-MA",
     genres: ["Drama", "Western"],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/xsiecCxd8lkcAluw0wWwbW5CwSv.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/9gWv6oZcE8R7N3F5H5B0c1S6r6M.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/wowtdtDM980GjuxCXJY4seeqP2O.jpg",
     videoUrl: "123456",
     overview: "The series follows fan-favorite characters Beth Dutton and Rip Wheeler as they leave Montana and attempt to build a new life in South Texas. Their fresh start is quickly complicated by a ruthless rival ranch and the persistent shadows of their past.",
     director: "Taylor Sheridan",
