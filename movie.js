072a76f4fbb4f01b0c4a8c662cf2e84789155b50
RandiBajat <randibajat@github.com>
2026-08-22 19:40:29 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -27,7 +27,7 @@
 const FEATURED_TITLES = ["Reacher","Lanterns", "House of the Dragon", "The Invite", "Spider-Man: Brand New Day", "The Odyssey", "Obsession", "The Last House","Silo"];
 const TRENDING_TITLES = ["Reacher","Lanterns", "Spider-Man: Brand New Day", "The Odyssey", "Minions & Monsters", "The Invite", "Young Washington", "The Last House", "Ted Lasso", "Project Hail Mary"];
 const TRENDING_THIS_WEEK_MOVIES = ["Spider-Man: Brand New Day","The Odyssey","Obsession","Minions & Monsters","The Last House","Disclosure Day","The Invete","The End of Oak Street","Backrooms","Camp Rock 3","Evil Dead Burn","Project Hail","Supergirl"]; // Add titles here for 'Trending Movies This Week'
-const TRENDING_THIS_WEEK_SERIES = ["Lanterns","Reacher","Lucky","Silo","Ted Lasso",""]; // Add titles here for 'Trending Series This Week'
+const TRENDING_THIS_WEEK_SERIES = ["Lanterns","Reacher","Lucky","Silo","Ted Lasso","X-Men '97"]; // Add titles here for 'Trending Series This Week'
 
 // ==========================================
 // 2. MOVIE DATABASE (loaded from MongoDB)
