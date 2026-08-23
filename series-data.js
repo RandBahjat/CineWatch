96bc2e3d02f7d1969f4f66ecc54d43a11b304273
RandiBajat <randibajat@github.com>
2026-08-23 23:18:11 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -4,33 +4,6 @@
 
 window._SERIES_DATA = [
   {
-    title: 'One Piece',
-    type: 'Anime',
-    year: 1999,
-    rating: 9.0,
-    age: 'TV-14',
-    duration: '24m',
-    genres: ['Animation', 'Action', 'Adventure', 'Comedy', 'Fantasy'],
-    poster: 'https://image.tmdb.org/t/p/w600_and_h900_face/cMD9Ygz11zjJzAovURpO75Qg7rT.jpg',
-    backdrop: 'https://image.tmdb.org/t/p/original/mSXMWkGAwz2tF9nE7aE9Jk0N9V6.jpg',
-    videoUrl: '37854',
-    overview: "Monkey D. Luffy refuses to let anyone or anything stand in the way of his quest to become the king of all pirates. With a course charted for the treacherous waters of the Grand Line and beyond, this is one captain who'll never give up until he's claimed the greatest treasure on Earth: the Legendary One Piece!",
-    director: 'Eiichiro Oda',
-    cast: ['Mayumi Tanaka', 'Kazuya Nakai', 'Akemi Okamura', 'Kappei Yamaguchi', 'Hiroaki Hirata'],
-    trending: true,
-    featured: true,
-    is4k: false,
-    seasons: [
-      {
-        season: 1,
-        episodes: [
-          { episode: 1, title: "I'm Luffy! The Man Who Will Become the Pirate King!" },
-          { episode: 2, title: "The Great Swordsman Appears! Pirate Hunter Roronoa Zoro" }
-        ]
-      }
-    ]
-  },
-  {
     title: 'Lanterns',
     type: 'TV Show',
     year: 2026,
