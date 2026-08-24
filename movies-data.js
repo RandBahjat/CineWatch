aa4dcf5fee18b97e8ddc1a1c1a9ebb48a89323af
RandiBajat <randibajat@github.com>
2026-08-24 23:44:25 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10646,8 +10646,8 @@ window._MOVIES_DATA = [
 
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/nyXB28bVUcdWQ3CG8z4cy6BWhJs.jpg",
-    backdrop: "https://images2.alphacoders.com/931/thumb-1920-931970.jpg",
-    videoUrl: "545742",
+    backdrop: "https://image.tmdb.org/t/p/original/qLfL9tLekOvDNLvWrnJ6jTlsugt.jpg",
+    videoUrl: "",
     overview: "The Straw Hats embark on a dangerous trip to the sky, where their destination comes straight out of a fairytale — the island of Skypiea and its grand city of gold await among the clouds. But a clash of winged clans and a man who calls himself God won't make things easy; only Luffy can resist this God's mighty power and light the mysteries of the past.",
     director: "Tetsuya Endo",
     cast: [
