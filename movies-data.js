2b8d6105f9a3a2eade4f1543ab347b0d83ea1a73
RandiBajat <randibajat@github.com>
2026-08-25 00:19:28 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10941,7 +10941,7 @@ window._MOVIES_DATA = [
       "Fantasy",
 
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/wPBFqO9zeSjX3bK9NMvhm9xL1Le.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/yS5z6jMr5reJN6WwtBQcnKQ8b75.jpg",
     backdrop: "https://m.media-amazon.com/images/M/MV5BMDg3ZTIzNDItOWYxMC00ZDM0LTg2OWYtODg2ZjkwNWZmNzUxXkEyXkFqcGc@._V1_.jpg",
     videoUrl: "215180",
     overview: "Remake special retelling Arlong Park arc. Navigator Nami leaves Straw Hat crew to return home to Cocoyasi Village. Luffy and friends follow Nami, discovering she works for tyrannical fishman Arlong to buy back village freedom. When Arlong betrays deal, Nami asks Luffy for help, leading to grand battle to liberate village and secure Nami place as crew navigator.",
