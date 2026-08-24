db41d939c05bdc2941b93ea2dd97ee1e3a37d2fd
RandiBajat <randibajat@github.com>
2026-08-24 20:32:23 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10464,7 +10464,7 @@ window._MOVIES_DATA = [
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/8ibfhe4P7rhmn3lrPhOZzIJHA2B.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/ixrYaoKtLO6HpciDbb4QGj3D3tT.jpg",
     videoUrl: "900667",
-    overview: "Damian Wayne, new Robin, struggle fit in with Teen Titans, sent join team by Batman. Meanwhile demon Trigon threaten possess Justice League, turn them evil, force young hero step up, save world.",
+    overview: "Uta, world's most celebrated singer, hide secret — she's Shanks' daughter. She hold concert, gather huge crowd including Straw Hat crew, but her true past + hidden power threaten to unravel everything.",
     director: "Sam Liu",
     cast: [
       "Stuart Allan (Damian Wayne/Robin), Taissa Farmiga (Raven), Kari Wahlgren (Starfire), Jason O'Mara (Batman), Sean Maher (Nightwing)"
