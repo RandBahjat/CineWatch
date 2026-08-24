e5a085231e677c8356b6b56bc0425938692137a8
RandiBajat <randibajat@github.com>
2026-08-25 00:31:18 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -52871,8 +52871,7 @@ window._SERIES_DATA = [
           { episode: 5, title: "Eat at Baratie!" },
           { episode: 6, title: "The Chef and the Chore Boy" },
           { episode: 7, title: "The Girl with the Sawfish Tattoo" },
-          { episode: 8, title: "Whiskey Limits" },
-          { episode: 9, title: "El Padrino" }
+          { episode: 8, title: "Worst in the East" },
         ]
       }
     ]
