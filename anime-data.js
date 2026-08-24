1b42ca715250895d12dcab714cb0b670e88d26ee
RandiBajat <randibajat@github.com>
2026-08-25 00:44:59 +0300

    Auto-sync updates

--- anime-data.js
+++ anime-data.js
@@ -6048,7 +6048,7 @@ window._ANIME_DATA = [
     ],
     "poster": "https://www.themoviedb.org/t/p/w600_and_h900_face/dB4EDhre2dsC2kxYDavyKWqLQwi.jpg",
     "backdrop": "https://netflixjunkie-media.b-cdn.net/media/image/image_1742647488887.jpeg",
-    "videoUrl": "37854",
+    "videoUrl": "129",
     "anilistId": 21,
     "animeSlug": "one-piece",
     "overview": "Monkey D. Luffy refuses to let anyone or anything stand in the way of his quest to become the king of all pirates. With a course charted for the treacherous waters of the Grand Line and beyond, this is one captain who'll never give up until he's claimed the greatest treasure on Earth: the Legendary One Piece!",
