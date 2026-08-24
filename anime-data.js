938743c91f948903c45bf4e4b0d4ca8cb8503404
RandiBajat <randibajat@github.com>
2026-08-25 01:04:16 +0300

    Auto-sync updates

--- anime-data.js
+++ anime-data.js
@@ -6046,7 +6046,7 @@ window._ANIME_DATA = [
         "Sci-Fi",
         "Fantasy"
     ],
-    "poster": "https://www.themoviedb.org/t/p/w600_and_h900_face/dB4EDhre2dsC2kxYDavyKWqLQwi.jpg",
+    "poster": "https://www.themoviedb.org/t/p/w600_and_h900_face/oQ5CnVj3TRifXl2bIOri6H6rfNe.jpg",
     "backdrop": "https://image.tmdb.org/t/p/original/zOz6DeXeOhKRVGUuiK3WufLXQIB.jpg",
     "videoUrl": "12971",
     "anilistId": 813,
