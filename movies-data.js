0185f69b09dcb13eed627548d2168bdc46848d4d
RandiBajat <randibajat@github.com>
2026-08-25 00:03:28 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10783,7 +10783,7 @@ window._MOVIES_DATA = [
     backdrop: "https://image.tmdb.org/t/p/original/xSvFPxS4JR2G6oRmgmeq9oSIJR4.jpg",
     videoUrl: "541867",
     overview: "Experience the inspiring moments that launched Luffy and the first four members of his crew on their grand adventure to find the One Piece — from Shanks giving Luffy the straw hat, to Nami facing the Fish-Men at her hometown. Zoro finds his swordsman's zeal, Usopp pays for his pranks, and Sanji sizzles up the Baratie, all in the waters where it began.",
-    director: "Tatsuya Nagamine",
+    director: "Takashi Otsuka",
     cast: [
       "Mayumi Tanaka (Luffy), Kazuya Nakai (Zoro), Akemi Okamura (Nami), guest star Shun Oguri (Mad Treasure)"
     ],
