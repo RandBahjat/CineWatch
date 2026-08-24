9608616245ac8e33fa07181903f8158b740961eb
RandiBajat <randibajat@github.com>
2026-08-24 20:34:24 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10485,7 +10485,7 @@ window._MOVIES_DATA = [
       "Action",
       "Animation",
       "Adventure",
-      "Fantasy",
+      "Fantasy"
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/8ibfhe4P7rhmn3lrPhOZzIJHA2B.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/ixrYaoKtLO6HpciDbb4QGj3D3tT.jpg",
