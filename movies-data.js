a2411e86b9184320b2f08a956030416130b2757e
RandiBajat <randibajat@github.com>
2026-08-24 20:37:59 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10526,6 +10526,32 @@ window._MOVIES_DATA = [
     is4k: false,
     seasons: []
   },
+  {
+    title: "One Piece Film: GOLD",
+    type: "Movie",
+    year: 2016,
+    rating: 7.1,
+    age: "TV-14",
+    duration: "2h 4m",
+    genres: [
+      "Action",
+      "Animation",
+      "Adventure",
+      "Fantasy"
+    ],
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/9PgiOFTLZXP7emlwcIt0yRasJ9h.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/9EfmaA6S9p33rqt3RybcLRfOUMr.jpg",
+    videoUrl: "374205",
+    overview: "The glittering Gran Tesoro, a floating city of casinos and entertainment beyond government law, draws pirates, Marines, and the ultra-rich alike. Chasing dreams of hitting the jackpot, Luffy and crew sail straight into the city — but behind its gold-plated walls waits Gild Tesoro, a ruthless tycoon whose deep pockets hide even deeper ambitions.",
+    director: "Hiroaki Miyamoto",
+    cast: [
+      "Mayumi Tanaka (Luffy), Kazuya Nakai (Zoro), Akemi Okamura (Nami), Kazuhiro Yamaji (Gild Tesoro)"
+    ],
+    trending: false,
+    featured: false,
+    is4k: false,
+    seasons: []
+  },
 
 
 
