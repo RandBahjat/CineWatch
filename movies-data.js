00aae8666f2af7bf71f65bca0a00ea058b157146
RandiBajat <randibajat@github.com>
2026-08-25 00:08:47 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10829,7 +10829,7 @@ window._MOVIES_DATA = [
       "Action",
       "Animation",
       "Adventure",
-      ""
+      "Comedu"
 
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/caUI7YlhVXykFWQ7Ul7RQ2wQRpv.jpg",
