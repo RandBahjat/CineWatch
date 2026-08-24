ba07145b8e4612e898791c95b591e8f8af99bc43
RandiBajat <randibajat@github.com>
2026-08-24 23:56:41 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10742,7 +10742,7 @@ window._MOVIES_DATA = [
     title: "One Piece: Heart of Gold",
     type: "Movie",
     year: 2016,
-    rating: 7.1,
+    rating: 6.9,
     age: "TV-14",
     duration: "1h 35m",
     genres: [
