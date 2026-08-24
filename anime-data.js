57ba0599666fce53743289571647b07758beb2d7
RandiBajat <randibajat@github.com>
2026-08-24 16:35:35 +0300

    Auto-sync updates

--- anime-data.js
+++ anime-data.js
@@ -19,7 +19,7 @@ window._ANIME_DATA = [
     ],
     "poster": "https://www.themoviedb.org/t/p/w600_and_h900_face/dB4EDhre2dsC2kxYDavyKWqLQwi.jpg",
     "backdrop": "https://image.tmdb.org/t/p/original/2rmK7mnchw9Xr3XdiTFSxTTLXqv.jpg",
-    "videoUrl": "37854",
+    "videoUrl": "21",
     "overview": "Monkey D. Luffy refuses to let anyone or anything stand in the way of his quest to become the king of all pirates. With a course charted for the treacherous waters of the Grand Line and beyond, this is one captain who'll never give up until he's claimed the greatest treasure on Earth: the Legendary One Piece!",
     "director": "Eiichiro Oda",
     "cast": [
