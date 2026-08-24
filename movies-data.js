92434f457c0286036bac5e30db909a20e89ed639
RandiBajat <randibajat@github.com>
2026-08-24 23:52:06 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10698,7 +10698,7 @@ window._MOVIES_DATA = [
       "Comedy"
 
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/8tqXFF48K3AB089yAmMA4ENzyor.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/phrVSY5cpUkybc0gU41crWi2XIP.jpg",
     backdrop: "https://occ-0-2219-2218.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABb5euxszRm0NUiAX9ys35taWOTzzU1SjEbkCGg0dNr8N5zB1rggPyBqsAgzlbspbWA2BO_Z_Bcl761DKOe0CEvMhVfJ1IB9pBS3A.jpg?r=2ba",
     videoUrl: "760048",
     overview: "The Straw Hat Pirates are on a quest to save the desert kingdom of Alabasta. A civil war brews among the sands, one started and stirred by Crocodile and his corrupted Baroque Works gang. The stakes run ever higher as Princess Vivi's homeland threatens to tear itself apart.",
