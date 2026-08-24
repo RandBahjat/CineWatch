58c5fa1d5cdcbe5af02d1e2690409fa027f4ee6d
RandiBajat <randibajat@github.com>
2026-08-25 00:13:38 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10861,8 +10861,8 @@ window._MOVIES_DATA = [
 
     ],
     poster: "https://static.wikia.nocookie.net/anime-database/images/f/fd/OnePieceSPC3.jpg/revision/latest?cb=20200128103029",
-    backdrop: "https://image.tmdb.org/t/p/original/eiiMkub2ySbDqrMnrn5swhSsZNI.jpg",
-    videoUrl: "44731",
+    backdrop: "https://images.plex.tv/photo?size=large-1920&scale=1&url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2F6Sa3iAcZcPW1mM35112m9hXHHLh.jpg",
+    videoUrl: "",
     overview: "When Nami falls gravely ill, the Straw Hats seek help on Drum Island, where they meet reindeer doctor Tony Tony Chopper and clash with the tyrannical Wapol Pirates. A what-if retelling of how Chopper joined the crew — uniquely set with Franky, Nico Robin, and the Thousand Sunny already part of the story, and an original villain, Musshuru.",
     director: "Junji Shimizu",
     cast: [
