f476204950a4f7117f93b420b615ea90ca502392
RandiBajat <randibajat@github.com>
2026-08-24 23:46:55 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10664,7 +10664,7 @@ window._MOVIES_DATA = [
     year: 2007,
     rating: 6.7,
     age: "PG-13",
-    duration: "43m",
+    duration: "1h 30m",
     genres: [
       "Action",
       "Animation",
