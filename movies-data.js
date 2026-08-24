a1f613c938fe0b87a9b5146e735d36a8f9c51a10
RandiBajat <randibajat@github.com>
2026-08-24 23:55:56 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -10728,7 +10728,7 @@ window._MOVIES_DATA = [
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/tEJBIHzaW1IoOVHtgblXy76kj4G.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/41e5H3UhT7D8VYBh4rdGy5fB0lv.jpg",
     videoUrl: "44727",
-    overview: "While relaxing on a peaceful beach, the Straw Hat crew's ship, the Going Merry, gets hijacked — along with Luffy's precious straw hat and Nami taken hostage. Teaming up with the mysterious Thief Brothers, Luffy, Zoro, Sanji, and Usopp track the culprits to Clockwork Island, where the tyrannical Bear King plots to conquer the seas with a superweapon.",
+    overview: "Broke after Luffy's huge appetite drains the crew's funds, the Straw Hats stumble onto a secret pirate race called the Dead End Competition, with a 300,000,000 Beli prize. Nami signs them up on the spot — but the race pits them against dangerous rivals, including a bounty hunter and the ruthless ex-Marine-turned-pirate Captain Gasparde.",
     director: "Junji Shimizu",
     cast: [
       "Mayumi Tanaka (Luffy), Kazuya Nakai (Zoro), Akemi Okamura (Nami), Kappei Yamaguchi (Usopp), Hiroaki Hirata (Sanji)"
