674bc5af6b7ba71d6978d73b4cdd249b94d646c7
RandiBajat <randibajat@github.com>
2026-08-25 12:02:45 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -262,6 +262,30 @@ window._MOVIES_DATA = [
     seasons: []
   },
   {
+    title: "Evil Dead Burn",
+    type: "Movie",
+    year: 2026,
+    rating: 6.4,
+    age: "R",
+    duration: "1h 50m",
+    genres: [
+      "Horror",
+      "Supernatural"
+    ],
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/uRxrNXQWkHoENm3nwVOZDYSCx2F.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/hkRv7WQYMbccuay0tBZPS8axvjd.jpg",
+    videoUrl: "1212763",
+    overview: "The sixth installment in the legendary horror franchise, serving as a direct sequel to Evil Dead Rise. The story follows a young woman named Alice who seeks comfort from her in-laws at a secluded country home following the sudden death of her husband. However, the grieving gathering quickly devolves into a grueling family reunion from hell as members of the household are systematically possessed and transformed into bloodthirsty Deadites.",
+    director: "Sébastien Vaniček",
+    cast: [
+      "Souheila Yacoub (as Alice), Hunter Doohan (as Joseph), Luciane Buchanan (as Thya), Tandi Wright (as Susan), Erroll Shand (as Edgar), Maude Davey (as Polly)"
+    ],
+    trending: false,
+    featured: false,
+    is4k: false,
+    seasons: []
+  },
+  {
     title: "The Dark Knight Rises",
     type: "Movie",
     year: 2012,
