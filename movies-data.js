894c59f5c673ade8da0956caa832dc1492252447
RandiBajat <randibajat@github.com>
2026-08-25 12:47:46 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -146,7 +146,7 @@ window._MOVIES_DATA = [
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/9pKPTugulAt0EtH0g4gvJGffPSg.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/caBIySpwuFi2i7ynvHIlnxJLOdN.jpg",
-    videoUrl: "",
+    videoUrl: "156",
     overview: "After years of brutal war, Odysseus sets sail for home — but the gods have other plans. Crossing a treacherous Mediterranean, he and his crew face the Cyclops Polyphemus, the deadly call of the Sirens, and the sorceress Circe, each trial pushing him closer to the edge of what he can endure to see Ithaca again.",
     director: "Christopher Nolan",
     cast: [
