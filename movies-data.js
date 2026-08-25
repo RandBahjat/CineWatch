897fb091dfa54b2f6fde0591aeca38c860ffc8c8
RandiBajat <randibajat@github.com>
2026-08-25 23:57:17 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11323,6 +11323,33 @@ window._MOVIES_DATA = [
     is4k: false,
     seasons: []
   },
+  {
+    title: "Naruto the Movie: Ninja Clash in the Land of Snow",
+    type: "Movie",
+    year: 2004,
+    rating: 6.6,
+    age: "TV-14",
+    duration: "1h 22m",
+    genres: [
+      "Action",
+      "Animation",
+      "Adventure",
+      "Fantasy",
+
+    ],
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/eUNRUeSNzm8LktH4HRaYiAReB6R.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/bW0Hr19Xo7LDnMmywXuH5YoTMY5.jpg",
+    videoUrl: "16907",
+    overview: "Naruto team hire guard actress on movie shoot, discover she secretly princess of Land of Snow. Must protect her from evil warlord try seize throne, control land's power source.",
+    director: "Tensai Okamura",
+    cast: [
+      "Junko Takeuchi, Chie Nakamura, Noriaki Sugiyama, Kazuhiko Inoue, Hidekatsu Shibata"
+    ],
+    trending: false,
+    featured: false,
+    is4k: false,
+    seasons: []
+  },
 
 
 
