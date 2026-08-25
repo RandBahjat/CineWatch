9a5d2ef8c02551f12277677a980368b5369e6cca
RandiBajat <randibajat@github.com>
2026-08-25 23:43:45 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11095,7 +11095,7 @@ window._MOVIES_DATA = [
 
     ],
     poster: "https://image.tmdb.org/t/p/original/eJoVvWg9yR7rxxdyjlhLgR4vRjP.jpg",
-    backdrop: "https://media.themoviedb.org/t/p/w533_and_h300_face/niG1LY5qQwDG4S8l5CBAaFl2XjL.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/vEiDUw5a1wDZVR3PjZaq8AXhF3r.jpg",
     videoUrl: "347201",
     overview: "Boruto, Naruto's son, resent father for always busy being Hokage, never around. When new enemy attack during ninja tournament, Boruto must fight alongside father, learn what it really mean carry ninja legacy.",
     director: "Hiroyuki Yamashita",
