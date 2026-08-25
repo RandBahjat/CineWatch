daff42b459de25fc50a0adeae8aad667a6899f6d
RandiBajat <randibajat@github.com>
2026-08-26 00:06:09 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11417,7 +11417,7 @@ window._MOVIES_DATA = [
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/euPgyPsiNFp6gOBB7nUFxvN5ZaY.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/isrro0soStk2tSWMsI50lPPhUsU.jpg",
     videoUrl: "169917",
-    overview: "Henry Hill rise through ranks of mob from young age, live fast life of money, power, violence alongside Jimmy Conway, Tommy DeVito. Loyalty, greed, paranoia eat away at friendship, world start crumble around him.",
+    overview: "Ex-cop turn unlicensed private eye, Matt Scudder, hire by drug trafficker track down men who kidnap, kill his wife. Dig deep into dark case, uncover twisted pair of killer target family of criminal.",
     director: "Martin Scorsese",
     cast: [
       "Robert De Niro, Ray Liotta, Joe Pesci, Lorraine Bracco, Paul Sorvino"
