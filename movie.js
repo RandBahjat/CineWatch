341654557f88ee6256161060024d1f0113af567f
RandiBajat <randibajat@github.com>
2026-08-25 12:28:39 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -31,7 +31,7 @@ if (window.location.hash.includes("type=recovery")) {
 // ==========================================
 let FEATURED_TITLES = ["Mutiny","Reacher", "Lanterns", "House of the Dragon", "The Invite", "Spider-Man: Brand New Day", "The Odyssey", "Obsession", "The Last House", "Silo"];
 let TRENDING_THIS_WEEK_MOVIES = ["Mutiny","Spider-Man: Brand New Day", "The Odyssey", "Motor City","Toy Story 5","Obsession", "Minions & Monsters", "The Last House", "Disclosure Day", "The Invite", "The End of Oak Street", "Backrooms", "Camp Rock 3", "Evil Dead Burn", "Project Hail Mary", "Supergirl"]; // Add titles here for 'Trending Movies This Week'
-let TRENDING_THIS_WEEK_SERIES = ["Lanterns", "Reacher", "Lucky", "Silo", "Ted Lasso", "X-Men '97",""]; // Add titles here for 'Trending Series This Week'
+let TRENDING_THIS_WEEK_SERIES = ["Lanterns", "Reacher", "Lucky", "Silo", "Ted Lasso", "X-Men '97","Lionesss"]; // Add titles here for 'Trending Series This Week'
 const POPULAR_MOVIES = ["Spider-Man: Brand New Day", "The Odyssey", "Minions & Monsters", "The Invite", "Spider-Man: No Way Home", "The End of Oak Street", "Disclosure Day", "Camp Rock 3", "The Last House", "Michael", "Project Hail Mary"]; // Add titles here for 'Popular Movie'
 const POPULAR_SERIES = ["Reacher", "House of the Dragon", "Ted Lasso", "The Mentalist", "Lucky", "Off Campus", "Silo", "Game of Thrones", "The Sopranos", "Stranger Things", "The Boys"]; // Add titles here for 'Popular Series'
 
