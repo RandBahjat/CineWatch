26dddb6d50002428007230ddb2dd176363035f18
RandiBajat <randibajat@github.com>
2026-08-25 12:32:54 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -8446,7 +8446,8 @@ window._SERIES_DATA = [
     genres: [
       "Action",
       'Crime',
-      'Drama'
+      'Drama',
+      ""
     ],
     poster: 'https://image.tmdb.org/t/p/original/pE8CScObQURsFZ723PSW1K9EGYp.jpg',
     backdrop: 'https://image.tmdb.org/t/p/original/m0GVaV4mgE8aFSHp8kTevHb8feu.jpg',
