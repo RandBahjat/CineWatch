48ea1ff1f1cb7ce382ac23e1c66488b4a1cb445b
RandiBajat <randibajat@github.com>
2026-08-25 23:40:15 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11202,7 +11202,7 @@ window._MOVIES_DATA = [
       "Fantasy",
 
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/4WT7zYFpe0fsbg6TitppiHddWAh.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/bBqEiQbbfyt4MWR3NhDZMbS4Wp8.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/853Hj8f2d9nCSzIpYzPIxIv5uNK.jpg",
     videoUrl: "75624",
     overview: "Naruto frame for attack on Raikage, lock up in Hōzuki Castle, secret prison ran by mysterious warden. Must clear name, uncover real plot, escape before execution.",
