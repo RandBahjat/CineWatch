a76baed3794ca55a8d7b43106cc33bd22eadd357
RandiBajat <randibajat@github.com>
2026-08-26 00:05:28 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11414,7 +11414,7 @@ window._MOVIES_DATA = [
       "Mystery"
 
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/9OkCLM73MIU2CrKZbqiT8Ln1wY2.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/euPgyPsiNFp6gOBB7nUFxvN5ZaY.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/x1Ohy5LGhy67rRjbXUM1DNDAICA.jpg",
     videoUrl: "769",
     overview: "Henry Hill rise through ranks of mob from young age, live fast life of money, power, violence alongside Jimmy Conway, Tommy DeVito. Loyalty, greed, paranoia eat away at friendship, world start crumble around him.",
