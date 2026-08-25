fe3bb232f56ca97adc4f7977b3ff97563d3c34f4
RandiBajat <randibajat@github.com>
2026-08-26 00:04:53 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11401,6 +11401,31 @@ window._MOVIES_DATA = [
     is4k: false,
     seasons: []
   },
+  {
+    title: "GoodFellas",
+    type: "Movie",
+    year: 1990,
+    rating: 8.7,
+    age: "R",
+    duration: "2h 25m",
+    genres: [
+      "Crime",
+      "Drama"
+
+    ],
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/9OkCLM73MIU2CrKZbqiT8Ln1wY2.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/x1Ohy5LGhy67rRjbXUM1DNDAICA.jpg",
+    videoUrl: "769",
+    overview: "Henry Hill rise through ranks of mob from young age, live fast life of money, power, violence alongside Jimmy Conway, Tommy DeVito. Loyalty, greed, paranoia eat away at friendship, world start crumble around him.",
+    director: "Martin Scorsese",
+    cast: [
+      "Robert De Niro, Ray Liotta, Joe Pesci, Lorraine Bracco, Paul Sorvino"
+    ],
+    trending: false,
+    featured: false,
+    is4k: false,
+    seasons: []
+  },
 
 
 
