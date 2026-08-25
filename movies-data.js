15a849f01db90d3ae632bad4e17cd85d6ef23460
RandiBajat <randibajat@github.com>
2026-08-25 12:46:36 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -144,7 +144,7 @@ window._MOVIES_DATA = [
       "Animation",
       "Crime"
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/5rhTDKUhPYvpdQIijFIs5VoWsON.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/9pKPTugulAt0EtH0g4gvJGffPSg.jpg",
     backdrop: "odysseus-matt-damon-3840x2160-26432.jpg",
     videoUrl: "1368337",
     overview: "After years of brutal war, Odysseus sets sail for home — but the gods have other plans. Crossing a treacherous Mediterranean, he and his crew face the Cyclops Polyphemus, the deadly call of the Sirens, and the sorceress Circe, each trial pushing him closer to the edge of what he can endure to see Ithaca again.",
