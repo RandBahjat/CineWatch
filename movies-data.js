61f943f0996b3b1f2a9cd7d9b2d2e862367ebe39
RandiBajat <randibajat@github.com>
2026-08-25 12:16:32 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -559,6 +559,31 @@ window._MOVIES_DATA = [
     seasons: []
   },
   {
+    title: "The Last House",
+    type: "Movie",
+    year: 2026,
+    rating: 5.5,
+    age: "PG-13",
+    duration: "1h 52m",
+    genres: [
+      "Sci-Fi",
+      "Horror",
+      "Mystery"
+    ],
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/6JU7E8Vv2M11egkctWVOScxWR75.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/A2SsNDtRcz3qNhFoNNrbomTesBx.jpg",
+    videoUrl: "1284041",
+    overview: "A family of four is suddenly sealed inside their home with no way out. With every door and window rendered impossible to escape, their safe haven quickly transforms into a prison as supplies dwindle and an unseen force outside refuses to let them leave. As fear, desperation, and suspicion tear the family apart, they must work together to uncover the truth behind the mysterious phenomenon before time runs out.",
+    director: "Louis Leterrier",
+    cast: [
+      "Greta Lee, Wagner Moura, Riley Chung, Emma Ho, Noah Alexander Sosnowski, Gabriel Barbosa, Sam Lerner, Arden Cho"
+    ],
+    trending: false,
+    featured: false,
+    is4k: false,
+    seasons: []
+  },
+  {
     title: "Minions & Monsters",
     type: "Movie",
     year: 2026,
