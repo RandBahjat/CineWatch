8d15c3611afcbf749cf76f0616aac40d2d325ada
RandiBajat <randibajat@github.com>
2026-08-25 12:24:55 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -2474,7 +2474,7 @@ window._SERIES_DATA = [
           },
           {
             episode: 6,
-            title: "The Cat and the Bat'"
+            title: "The Lie Is the Truth'"
           },
           {
             episode: 7,
