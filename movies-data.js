942f533b05694acb5728d60e8a80792a9dd0fd68
RandiBajat <randibajat@github.com>
2026-08-26 12:58:49 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -12175,7 +12175,7 @@ window._MOVIES_DATA = [
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/7uRu9EA3nie0n2mlVDDLlTI3IzC.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/orROcqUdNe3rdepse6nskbxovXJ.jpg",
     videoUrl: "39108",
-    overview: "Powerful Namekian warlord Lord Slug arrive Earth, drain planet of energy, plan reshape it his own dark image. Goku must push past limit, tap into hidden power to stop him before Earth wither, die.",
+    overview: "Ancient evil warrior Tapion seal inside music box, wake up alongside monstrous beast Hirudegarn once he free. Trunks, Goten must earn Tapion's trust, work together stop Hirudegarn before it destroy world twice over.",
     director: "Mitsuo Hashimoto",
     cast: [
       "Masako Nozawa, Ryō Horikawa, Naoki Tatsuta, Naoko Watanabe, Hiromi Tsuru"
