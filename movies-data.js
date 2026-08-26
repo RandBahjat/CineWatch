a79a4188113e1fe2c3e4b198797543052bccdf4e
RandiBajat <randibajat@github.com>
2026-08-26 12:35:58 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11879,7 +11879,7 @@ window._MOVIES_DATA = [
 
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/hywuHoNTiDXp0M32LUa228EQ2br.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/y319VH0gIFie6KEO5L4nHhxxQUw.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/l6VxsJ3eZ9l99T8ayLs4CQJXZUR.jpg",
     videoUrl: "9437",
     overview: "Detective, psychologist Alex Cross join force with escape victim track down serial kidnapper hold group of women captive. Race against time uncover killer identity before he strike again, claim another life.",
     director: "Gary Fleder",
