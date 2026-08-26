c4817d850a6d17ff0b67f577c9f8373bfde2ecf1
RandiBajat <randibajat@github.com>
2026-08-26 13:10:44 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -12336,7 +12336,7 @@ window._MOVIES_DATA = [
 
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/dZCWRFJnwyxYOhzAhf7Xyru4moB.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/b3q3CZx1gf5CUWZCPjQm7OJz0B1.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/1hiaeEgYcVU3gA3nrPpcqJDZ4SV.jpg",
     videoUrl: "39106",
     overview: "Evil scientist clone Broly, create bio-engineer monster call Bio-Broly, unleash him at big tournament. Broly's berserk rage grow harder, harder to contain, threaten kill everyone trap inside island facility.",
     director: "Yoshihiro Ueda",
