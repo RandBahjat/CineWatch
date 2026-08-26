817dc42e74febfa3c511fe2109555706415e98bc
RandiBajat <randibajat@github.com>
2026-08-26 11:56:42 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11463,7 +11463,7 @@ window._MOVIES_DATA = [
     genres: [
       "Crime",
       "Mystery",
-      ""
+      "Comedy"
 
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/pbGveUC8mk0QRkdXpERPLbMFnrg.jpg",
