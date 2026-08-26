ddb149296cbd803b87103684c5f13d624dbb6f31
RandiBajat <randibajat@github.com>
2026-08-26 12:09:49 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11574,7 +11574,7 @@ window._MOVIES_DATA = [
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/nM26QosEfgjEegONCiNNrYMBTxD.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/rT40yt2rTZlAELcjPr8N5eqhZTN.jpg",
-    videoUrl: "",
+    videoUrl: "331962",
     overview: "Two young guy stumble into arm dealing business, land massive three hundred million dollar contract supply US military in Afghanistan war. Greed, ambition push them deeper into shady, dangerous world of international arms trade.",
     director: "Todd Phillips",
     cast: [
