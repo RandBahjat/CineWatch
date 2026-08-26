86dc072d738b90ee8d0f3d9516d25e7be65ff5e5
RandiBajat <randibajat@github.com>
2026-08-26 12:52:34 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -12091,7 +12091,7 @@ window._MOVIES_DATA = [
       "Fantasy"
 
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/nxZEdYcHMuD8SSuwusDnK9CD2H1.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/uMEgkyiPznZP5AiMSWAk2jsj5gC.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/61t00k4ta1nNqKI2qPLKgoW6EoP.jpg",
     videoUrl: "126963",
     overview: "God of Destruction Beerus wake from long slumber, hunt for legendary Super Saiyan God he dream about. Goku, friend must find way awaken this new form before Beerus destroy Earth out boredom, rage.",
