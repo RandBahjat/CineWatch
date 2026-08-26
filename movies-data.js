eb465cab1c6a3bacfaabc77e4dd933c0322f21a9
RandiBajat <randibajat@github.com>
2026-08-26 12:03:08 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -11520,8 +11520,8 @@ window._MOVIES_DATA = [
 
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/qpPMewlugFaejXjz4YNDnpTniFX.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/uumksY17NYEONUHfSc9D1rzQ3zI.jpg",
-    videoUrl: "263472",
+    backdrop: "https://image.tmdb.org/t/p/original/mzFlwK2WN5KGCgEc4ExJmooJy97.jpg",
+    videoUrl: "",
     overview: "Family man home alone for weekend, two young woman show up his door, claim stuck in storm, need help. Let them in start chain of manipulation, seduction, terror he never see coming.",
     director: "Eli Roth",
     cast: [
