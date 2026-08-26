49eef1ae1882321833c14c952da902a3f06b09b4
RandiBajat <randibajat@github.com>
2026-08-26 12:55:24 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -12121,7 +12121,7 @@ window._MOVIES_DATA = [
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/iihTK9Af8G1ZzBjkIIAV4qQMkzF.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/h5Rvh7TUOLqmY2AOebwSEnHolwB.jpg",
     videoUrl: "39105",
-    overview: "Ancient legend of Saiyan monster Broly turn real, awaken on remote planet by Frieza's scheme. Goku, Vegeta must team up face Broly's near-limitless power, uncover truth of Saiyan history long buried.",
+    overview: "Vicious space pirate Bojack, freed after long imprisonment, seek revenge, wreak havoc during World Martial Arts Tournament. Young Gohan must step up, protect friend, fight fierce battle while Goku stuck away in Other World.",
     director: "Tatsuya Nagamine",
     cast: [
       "Masako Nozawa, Ryō Horikawa, Bin Shimada, Kōichi Yamadera, Aya Hisakawa"
