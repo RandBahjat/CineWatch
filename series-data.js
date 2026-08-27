31fe24a56f50547501474cc609373510d561d656
RandiBajat <randibajat@github.com>
2026-08-27 19:27:19 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -6,9 +6,9 @@ window._SERIES_DATA = [
   {
     title: 'Shameless',
     type: 'series',
+    rating: 8.5,
     poster: 'https://www.themoviedb.org/t/p/w600_and_h900_face/ifo31fMWLmyOVpdak9K0kY4jldQ.jpg',
     backdrop: 'https://image.tmdb.org/t/p/original/8A071YZoKIzoXt4t6aPWEyyuoxw.jpg',
-    rating: 8.5,
     genres: ['Drama', 'Comedy'],
     year: '2011',
     duration: '45m',
