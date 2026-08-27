37c7fbb4a076daebe9354665eb3c7ea17ef08069
RandiBajat <randibajat@github.com>
2026-08-27 20:47:17 +0300

    Auto-sync updates

--- anime-data.js
+++ anime-data.js
@@ -15,7 +15,7 @@ window._ANIME_DATA = [
       "Fantasy"
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/i2EEr2uBvRlAwJ8d8zTG2Y19mIa.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/uc8v0m8aYgO5Ff8uE7d1L0TksYx.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/zJVqkxZ2cSD01MDhoFbT5Hg9pqm.jpg",
     videoUrl: "46298",
     anilistId: 11061,
     animeSlug: "hunter-x-hunter-2011",
