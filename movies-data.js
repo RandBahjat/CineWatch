6532e2ab208c466b161178c6509fc9a38c24e56d
RandiBajat <randibajat@github.com>
2026-08-28 14:00:26 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -4313,7 +4313,7 @@ window._MOVIES_DATA = [
     year: 2019,
     rating: 8.1,
     age: "PG-13",
-    duration: "2h 38m",
+    duration: "2h 32m",
     genres: [
       "Action",
       "Thriller"
