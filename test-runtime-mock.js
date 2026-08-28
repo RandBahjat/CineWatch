5f87fd885c66e38567a4ca46d55d6a089b9323f0
RandiBajat <randibajat@github.com>
2026-08-28 14:53:28 +0300

    Auto-sync updates

---
+++ test-runtime-mock.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+global.window = { location: { hash: '' } };
+global.document = {
+    getElementById: () => ({ style: {}, classList: { toggle: () => {} }, addEventListener: () => {} }),
+    querySelectorAll: () => ([]),
+    querySelector: () => ({ classList: { toggle: () => {}, remove: () => {} }, style: {}, addEventListener: () => {} }),
+    body: { classList: { add: () => {}, remove: () => {} } },
+    addEventListener: () => {}
+};
+global.performance = { now: () => 0 };
+global.localStorage = { getItem: () => null, setItem: () => {} };
+global.setTimeout = (cb) => cb();
+
+try {
+    eval(fs.readFileSync('movies-data.js', 'utf8'));
+    eval(fs.readFileSync('series-data.js', 'utf8'));
+    eval(fs.readFileSync('anime-data.js', 'utf8'));
+    eval(fs.readFileSync('movie.js', 'utf8'));
+    
+    // Call init if it exists
+    if (typeof init === 'function') init();
+    console.log("Runtime check passed successfully.");
+} catch (e) {
+    console.error("Runtime error:");
+    console.error(e.stack);
+}
