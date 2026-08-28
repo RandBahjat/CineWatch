a9c147ed9e2e4b25389121542db6ec67ecf6c4e7
RandiBajat <randibajat@github.com>
2026-08-28 14:21:26 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -2801,7 +2801,7 @@ window._MOVIES_DATA = [
       "Sci-Fi"
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/wFjboE0aFZNbVOF05fzrka9Fqyx.jpg",
-    backdrop: "https://image.tmdb.org/t/p/original/yuvFyfOAO2UB5YP0HKgu8imtJul.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/y1zfDhKOMAtWhTrpaCVnjj3R4hX.jpg",
     videoUrl: "335787",
     overview: "Young street-smart thief Nathan Drake team up with seasoned treasure hunter Victor Sullivan, chase legendary lost gold once belong to Magellan's crew. Global adventure, deadly rival, ancient puzzle push them race against time, dangerous enemy.",
     director: "Ruben Fleischer",
