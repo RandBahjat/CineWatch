2dcaa041733fc817b86327cb5d1adbb17e19cee7
RandiBajat <randibajat@github.com>
2026-08-28 14:20:50 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -2793,7 +2793,7 @@ window._MOVIES_DATA = [
     year: 2022,
     rating: 6.7,
     age: "PG-13",
-    duration: "1h 56m",
+    duration: "1h 46m",
     genres: [
       "Action",
       "Adventure",
