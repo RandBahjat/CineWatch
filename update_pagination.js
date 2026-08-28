08613143b52e28d827dffbc92b3f477db4f75382
RandiBajat <randibajat@github.com>
2026-08-28 22:36:09 +0300

    Auto-sync updates

--- update_pagination.js
+++ update_pagination.js
@@ -1,25 +1,18 @@
 const fs = require('fs');
 let code = fs.readFileSync('movie.js', 'utf8');
 
-const target = `  // Add jump to page input
-  html += \`
-    <div class="page-jump">
-      <button class="page-btn page-jump-btn" id="\${paginationId}-jump-btn">Go</button>
-    </div>
-  \`;`;
-
-const rep = `  // Add jump to page input
+code = code.replace(
+  /\/\/\s*Add jump to page input[\s\S]*?container\.innerHTML\s*=\s*html;/,
+  `// Add jump to page input
   html += \`
     <div class="page-jump">
       <input type="number" class="page-jump-input notranslate" translate="no" id="\${paginationId}-jump-input" min="1" max="\${totalPages}" placeholder="\${goText}" title="Jump to page">
       <button class="page-btn page-jump-btn notranslate" translate="no" id="\${paginationId}-jump-btn">\${goText}</button>
     </div>
-  \`;`;
+  \`;
 
-if (code.includes(target)) {
-  code = code.replace(target, rep);
-  fs.writeFileSync('movie.js', code, 'utf8');
-  console.log('Successfully updated pagination jump input');
-} else {
-  console.log('Target string not found');
-}
+  container.innerHTML = html;`
+);
+
+fs.writeFileSync('movie.js', code, 'utf8');
+console.log('Regex replace complete');
