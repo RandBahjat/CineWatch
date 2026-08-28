e7b2e06310d5aa346b5e7aa5dcecf98aac2d73ed
RandiBajat <randibajat@github.com>
2026-08-28 21:15:38 +0300

    Auto-sync updates

--- scratch_fix.js
+++ scratch_fix.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
-const oldContent = fs.readFileSync('old_movie.js', 'utf8');
+const buf = fs.readFileSync('old_movie.js');
+const oldContent = (buf[0] === 0xFF && buf[1] === 0xFE) ? buf.toString('utf16le') : buf.toString('utf8');
 
 const startMarker = 'function openDetailsModal(movieId)';
-const endMarker = 'function fetchRawStream';
+const endMarker = 'async function fetchRawStream(';
 
 const startIdx = oldContent.indexOf(startMarker);
 const endIdx = oldContent.indexOf(endMarker);
@@ -25,11 +26,10 @@ if (startIdx !== -1 && endIdx !== -1) {
   let currentMovie = fs.readFileSync('movie.js', 'utf8');
   const curStart = currentMovie.indexOf(startMarker);
   const curEnd = currentMovie.indexOf(endMarker);
+  console.log('curStart:', curStart, 'curEnd:', curEnd);
   if (curStart !== -1 && curEnd !== -1) {
     currentMovie = currentMovie.substring(0, curStart) + modalCode + currentMovie.substring(curEnd);
     fs.writeFileSync('movie.js', currentMovie, 'utf8');
     console.log('Successfully updated movie.js');
-  } else {
-    console.error('Markers not found in movie.js, curStart:', curStart, 'curEnd:', curEnd);
   }
 }
