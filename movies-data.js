11e13fb616e9bc62efa7195de17763b2078a75f1
RandiBajat <randibajat@github.com>
2026-08-28 14:01:20 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -4320,7 +4320,7 @@ window._MOVIES_DATA = [
       "Sport",
       "Biograpghy"
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/dQgIcW6Th08kMRf2HBoYWoFE6OD.jpg",
+    poster: "https://image.tmdb.org/t/p/original/j3X6EgrPFc9BUKgMoMOoSkHt8iX.jpg",
     backdrop: "https://m.media-amazon.com/images/S/pv-target-images/f5a62dc8ead401c75012a3398a02c2ecb09d1f8f12aa6ba6cc04287c60c2b972.jpg",
     videoUrl: "1122573",
     overview: "Two extraction specialists are tasked with planning an escape route for a high-level female negotiator caught in the middle of a high-stakes, dangerous environment where the lines between friend and foe are blurred.",
