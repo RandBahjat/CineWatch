3c2330dcb6c790556b025b90112a99d3ccfdb715
RandiBajat <randibajat@github.com>
2026-08-28 14:51:14 +0300

    Auto-sync updates

---
+++ check-syntax.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const vm = require('vm');
+
+function checkFile(filename) {
+    try {
+        const content = fs.readFileSync(filename, 'utf8');
+        new vm.Script(content, { filename });
+        console.log(filename + " is OK");
+    } catch (e) {
+        console.error("Syntax error in " + filename + ":");
+        console.error(e.message);
+        if (e.stack) {
+            const firstLine = e.stack.split('\n')[0];
+            console.error("Location: " + firstLine);
+        }
+    }
+}
+
+checkFile('movies-data.js');
+checkFile('movie.js');
+checkFile('series-data.js');
+checkFile('anime-data.js');
