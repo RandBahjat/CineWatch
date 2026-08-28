d3c4c5044a4663eec750142c499957a9d6fd91fc
RandiBajat <randibajat@github.com>
2026-08-28 14:31:01 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -12669,7 +12669,7 @@ window._MOVIES_DATA = [
     genres: [
       "Action",
       "Adventure",
-      "Fantasy"
+      "S"
 
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/s4Qn5LF6OwK4rIifmthIDtbqDSs.jpg",
