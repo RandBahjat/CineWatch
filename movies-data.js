104f8cc3d06925c67e7bd2f78a8dca97f431a23f
RandiBajat <randibajat@github.com>
2026-08-28 13:20:51 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -12466,7 +12466,7 @@ window._MOVIES_DATA = [
       "Comedy"
 
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/fDX4Dp8IKvjBAaEb5MOJrGkxWX0.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/lYWEXbQgRTR4ZQleSXAgRbxAjvq.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/9mZGGJJP04RBmrzh9XVSfqRXl0d.jpg",
     videoUrl: "39101",
     overview: "Mysterious alien tree land on Earth, drain planet life force feed its deadly fruit, alien crew guard it fiercely. Goku, Gohan, Piccolo, Krillin must stop tree before it suck Earth dry, kill everyone on it.",
