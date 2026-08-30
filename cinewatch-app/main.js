9f6794e0564efc7f777258971f19cf1a03e5b24d
RandiBajat <randibajat@github.com>
2026-08-31 00:33:44 +0300

    Auto-sync updates

--- cinewatch-app/main.js
+++ cinewatch-app/main.js
@@ -19,7 +19,7 @@ function createWindow() {
     }
   });
 
-  mainWindow.loadURL('https://cinewatch.vercel.app/');
+  mainWindow.loadURL('https://cinewatch.watch/');
 
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
     if (url.startsWith('http:') || url.startsWith('https:')) {
