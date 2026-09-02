f80ae2f0743c04300eb0704f224348d6012699f9
RandiBajat <randibajat@github.com>
2026-09-02 16:43:06 +0300

    Auto-sync updates

--- cinewatch-app/movies-data.js
+++ cinewatch-app/movies-data.js
@@ -16,7 +16,7 @@ window._MOVIES_DATA = [
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/glALx6QaIgw1u4joXsnfHTjWi6D.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/tieojVpH6eW44JQOQEWnf8t4khG.jpg",
-    videoUrl: "860508",
+    videoUrl: ",
     overview: "Young son vanish, widower turn to estranged father, retired detective who once put away serial killer, The Whisper Man, terrorize quiet town Featherbank fifteen year back. Old case, new crime tangle together, dark rumor of accomplice resurface.",
     overviewKurdish: "کوڕی گەنجی پیاوێکی بێوەژن ون دەبێت لە فێدەربانک، شارۆچکەیەکی هێمن کە پازدە ساڵ لەمەوبەر لەلایەن پیاو کوژێک بە ناوی ویسپەر مان تۆقێندرابوو. ئەمە وای لێدەکات ڕوو لە باوکی لێدوورکەوتووی خۆی بکات، پۆلیسێکی خانەنشین کە ئەو کاتە پیاو کوژی گرتبوو. کەیسی کۆن و تاوانی نوێ تێکەڵ دەبنەوە، تاوتوێی نهێنی هاوکارێکی تاریک دووبارە سەرهەڵدەدات.",
     director: "James Ashcroft",
