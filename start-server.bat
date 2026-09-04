4e35134a8fc0188ce4af62e3ec4afae7283e8527
RandiBajat <randibajat@github.com>
2026-09-04 15:29:52 +0300

    Auto-sync updates

---
+++ start-server.bat
@@ -0,0 +1,8 @@
+@echo off
+title CineWatch Local Host Server
+cls
+echo ====================================================
+echo Starting CineWatch Local Host Server...
+echo ====================================================
+node server.js
+pause
