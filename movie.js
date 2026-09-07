0420cd18060149cf08467380778366aa060b8d6e
RandiBajat <randibajat@github.com>
2026-09-08 01:34:14 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -1848,7 +1848,7 @@ function renderUserBadge() {
     if (container) {
       container.innerHTML = `
         <button class="nav-user-icon-btn" id="headerLoginBtn" title="Sign In" aria-label="Sign In">
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.1" stroke-linecap="round" stroke-linejoin="round">
             <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/>
             <circle cx="12" cy="7" r="4"/>
           </svg>
