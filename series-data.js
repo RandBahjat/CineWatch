96d21865d702e214205589918e8ab35d7dfd621c
RandiBajat <randibajat@github.com>
2026-09-07 10:52:22 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -5383,6 +5383,7 @@ window._SERIES_DATA = [
     videoUrl: '236235',
     trailerUrl: "",
     overview: 'Fancy rich guy Eddie inherits big family estate after father die. But surprise! Massive underground cannabis weed empire hiding right under castle ground! Dangerous gang lords own farm. Eddie must play dirty crime game with bad guys to keep family safe and protect mansion.',
+    
     director: 'Craig Mazin, Neil Druckmann',
     cast: [
       'Pedro Pascal, Bella Ramsey, Anna Torv, Gabriel Luna, Storm Reid, Nick Offerman, Murray Bartlett, Isabela Merced, Young Mazino'
