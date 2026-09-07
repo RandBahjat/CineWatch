d63f7ea3bf6982e414b9f2ba31342feeae3fd076
RandiBajat <randibajat@github.com>
2026-09-07 11:06:38 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -45,7 +45,7 @@ window._MOVIES_DATA = [
     videoUrl: "1137844",
     trailerUrl: "https://youtu.be/om5Un9X720M?si=w2Q4Yb_O5B2js4Td",
     overview: "In 1987 Cold War time, American pilot crash land deep inside dangerous Soviet territory! To survive and get back home alive, pilot forced to team up with weird eccentric former KGB agent Nikolai Ustinov on wild buddy mission.",
-    overviewKurdish:"مایا مارتن، پارێزەرێکی بەتوانای لۆندۆنە. لە کاتی ڕاکردنی بەیانیان، کەسێکی نادیار ڕاڵێو و دەسەڵات دەگرێت بەسەر ئامێری ئەنسۆلینی کوڕە شەکرەدارەکەی. بۆ ڕزگارکردنی ژیانی کوڕەکەی، پێویستە مایا بە خێرایی لەنێو شاری لۆندۆن ڕابکات، گوێڕایەڵی داواکارییە مەترسیدارەکان بێت و شایەتحاڵێکی دادگا بکوژێت پێش ئەوەی کات تەواو بێت.",
+    overviewKurdish:لە سەردەمی جەنگی سارد لە ساڵی ١٩٨٧دا، فڕۆکەوانێکی ئەمریکی لە ناوچەیەکی دوورەدەست و مەترسیداری سۆڤیەتدا تێکدەشکێت و گیر دەخوات. بۆ ئەوەی لە ژیاندا بێنێتەوە و بگەڕێتەوە نیشتمانەکەی، ناچار دەبێت هاوکاری لەگەڵ نیکۆلای ئوستینۆڤ دروست بکات، کە ئەفسەرێکی پێشووی سەیر و سەمەرەی دەزگای سیخوڕی KGBیە.",
     director: "Kevin Macdonald",
     cast: ["Gal Gadot, Damian Lewis, Alfred Enoch, Rory Wilmot"],
     trending: true,
