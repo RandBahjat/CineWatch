ad2175995e52d8352df533e038c5d016ffb5b624
RandiBajat <randibajat@github.com>
2026-09-07 11:04:58 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -40,7 +40,7 @@ window._MOVIES_DATA = [
       "Adventure",
       "Comedy"
     ],
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/uxCaBoYXsDC4A0SqTm3SISj0OwK.jpg",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/hVXjX1jLZ1ljFSNGXpjJfbTUOa7.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/1KohYajglCHxTgj2NY2WTI7gn9v.jpg",
     videoUrl: "1386315",
     trailerUrl: "https://youtu.be/raY-TDLE458?si=kl8RoS6tBCeHwgEX",
