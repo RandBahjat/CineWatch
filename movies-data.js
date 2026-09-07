1893e5c79cf44eeed12f0fe7d2e7535fdf6c46e3
RandiBajat <randibajat@github.com>
2026-09-07 11:04:48 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -37,8 +37,8 @@ window._MOVIES_DATA = [
     duration: "1h 50m",
     genres: [
       "Action",
-      "",
-      "Family"
+      "Adventure",
+      "Comedy"
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/uxCaBoYXsDC4A0SqTm3SISj0OwK.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/1KohYajglCHxTgj2NY2WTI7gn9v.jpg",
