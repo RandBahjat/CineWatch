6876bcad0c648c269547301c4d1ccc46bf9391c3
RandiBajat <randibajat@github.com>
2026-09-07 10:55:12 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -5405,7 +5405,7 @@ window._SERIES_DATA = [
           },
           {
             episode: 3,
-            title: 'Long, Long Time'
+            title: 'Where's My Weed At?'
           },
           {
             episode: 4,
