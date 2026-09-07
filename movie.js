6253ddab0f10d95c1e47d167df85642b48152252
RandiBajat <randibajat@github.com>
2026-09-07 20:34:50 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -694,7 +694,7 @@ function updateHeroBanner() {
 function createMovieCardHTML(movie, rank = null, forcePoster = false) {
   const fav = isFavorite(movie.id);
   const primaryGenre = movie.genres && movie.genres.length > 0 ? translateGenre(movie.genres[0]) : "";
-  const displayType = movie.type || (movie.seasons ? "TV Show" : "Movie");
+  const displayType = movie.isAnime ? "Anime" : (movie.type || (movie.seasons ? "TV Show" : "Movie"));
   const rankHtml = rank !== null ? `
     <div class="top10-rank-badge">
       <span class="top10-rank-text">TOP</span>
@@ -721,7 +721,7 @@ function createMovieCardHTML(movie, rank = null, forcePoster = false) {
       <div class="card-details">
         <h4 class="card-title notranslate" translate="no">${movie.title}</h4>
         <div class="card-meta">
-          <span class="card-rating notranslate" translate="no">⭐ ${formatRating(movie.rating)}</span>
+          <span class="card-rating notranslate" translate="no"><span class="star-icon" style="color: #e50914; margin-right: 3px;">★</span>${formatRating(movie.rating)}</span>
           <span class="card-year notranslate" translate="no">${formatNumber(movie.year)}</span>
           <span class="card-type notranslate" translate="no">${formatMediaType(displayType)}</span>
         </div>
