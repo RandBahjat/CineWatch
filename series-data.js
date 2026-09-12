fddb6c32c8e52c5af9c2cc2611602d32ccd93735
RandiBajat <randibajat@github.com>
2026-09-13 02:32:59 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -18,7 +18,7 @@ window._SERIES_DATA = [
       'Mystery',
       'Thriller'
     ],
-    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_face/2mwgHfOyrFrmvozjFoEXHRdfGhv.jpg',
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_face/vozJZjHv0Yyv4PjoOxF5tVMUGFI.jpg',
     backdrop: 'https://image.tmdb.org/t/p/original/84PKxwkBn0OoH5Qj4uq97Nua0Q9.jpg',
     videoUrl: '258230',
     trailerUrl: "https://youtu.be/M8nHySkLb64?si=ncGOGxR7J7r25Gyh",
