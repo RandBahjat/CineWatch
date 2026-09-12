4113c0a37e7d92686afd9eb944435efa623ddb4b
RandiBajat <randibajat@github.com>
2026-09-12 17:28:44 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -21,7 +21,7 @@ window._MOVIES_DATA = [
     trailerUrl: "https://youtu.be/VSN8YM6urHo?si=56Fk_-YMpxANshNI",
     overview: "Group of ragtag misfit athletes assemble to save local community sports complex from greedy real estate developer. Underdog friends must train super hard, overcome crazy hurdles, and defeat arrogant champion rival team in high-stakes big tournament to win prize money and protect home turf!",
     overviewKurdish:"کۆمەڵێک وەرزشەوانی سەرنەکەوتوو و پەراوێزخراو پێکەوە کۆدەبنەوە بۆ ڕزگارکردنی سەنتەری وەرزشی ناوچەکەیان لە دەستی گەشەپێدەرێکی تەماحکاری خانوبەرە. یاریزانە لاوازەکان پێویستە بە سەختی مەشق بکەن، بەسەر ئاستەنگە شێتانەکاندا زاڵ ببن، و لە پاڵەوانێتییەکی گەورەی کۆتاییدا شکست بە تیپی ڕکابەری بەهێزیان بهێنن بۆ بردنی خەڵاتی دارایی و پاراستنی مەیدانەکەیان.",
-    director: "Kevin Macdonald",
+    director: "Paul Feig",
     cast: ["Gal Gadot, Damian Lewis, Alfred Enoch, Rory Wilmot"],
     trending: true,
     featured: true,
