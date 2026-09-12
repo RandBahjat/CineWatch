a9187215dd4b249fe62402a5b7dedf197aa99731
RandiBajat <randibajat@github.com>
2026-09-12 17:25:04 +0300

    Auto-sync updates

--- cinewatch-app/movies-data.js
+++ cinewatch-app/movies-data.js
@@ -12,6 +12,7 @@ window._MOVIES_DATA = [
     duration: "1h 33m",
     genres: [
       "Comedy",
+      "Family",
       "Family"
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/uxCaBoYXsDC4A0SqTm3SISj0OwK.jpg",
