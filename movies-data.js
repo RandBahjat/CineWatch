fb5f0a4f183ee113fcdd32b4818f4ade721ffb99
RandiBajat <randibajat@github.com>
2026-09-13 02:15:07 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -13952,10 +13952,7 @@ window._MOVIES_DATA = [
     age: "PG-13",
     duration: "1h 37m",
     genres: [
-      "Comedy",
-      "Adventure",
-      "Mystery",
-      "Sci-Fi"
+      "Comedy"
 
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/bYLM3GpNUZnoFElPXp1zlhDPdtv.jpg",
