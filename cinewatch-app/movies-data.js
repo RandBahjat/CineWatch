91813c81198c583275f12195aa0d7fa5be1efb9f
RandiBajat <randibajat@github.com>
2026-09-15 01:31:18 +0300

    Auto-sync updates

--- cinewatch-app/movies-data.js
+++ cinewatch-app/movies-data.js
@@ -395,8 +395,8 @@ window._MOVIES_DATA = [
     duration: "1h 50m",
     genres: [
       "Crime",
-      "Sci-Fi",
-      "Adventure"
+      "Drama",
+      "Thriller"
     ],
     poster: "https://image.tmdb.org/t/p/w600_and_h900_face/uhzRnTW4DM13UQBvZP3eVNzQTuz.jpg",
     backdrop: "https://images.hdqwalls.com/download/supergirl-team-up-4k-hb-3840x2160.jpg",
--- movies-data.js
+++ movies-data.js
@@ -396,7 +396,7 @@ window._MOVIES_DATA = [
     genres: [
       "Crime",
       "Drama",
-      "Adventure"
+      "Thriller"
     ],
     poster: "https://image.tmdb.org/t/p/w600_and_h900_face/uhzRnTW4DM13UQBvZP3eVNzQTuz.jpg",
     backdrop: "https://images.hdqwalls.com/download/supergirl-team-up-4k-hb-3840x2160.jpg",
