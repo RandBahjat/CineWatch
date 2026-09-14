aa4d89703f9328d46bdad6ee9cef8f6485aae8f2
RandiBajat <randibajat@github.com>
2026-09-15 00:46:57 +0300

    Auto-sync updates

--- cinewatch-app/movies-data.js
+++ cinewatch-app/movies-data.js
@@ -421,7 +421,7 @@ window._MOVIES_DATA = [
     title: "Avengers: Doomsday",
     type: "Movie",
     year: 2026,
-    rating: ,
+    rating: "TBR",
     age: "R",
     duration: "1h 19m",
     genres: [
