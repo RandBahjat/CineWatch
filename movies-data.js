76de839cf9a1ab9d4946a5682753b509bf7a6bbc
RandiBajat <randibajat@github.com>
2026-09-15 01:32:38 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -400,7 +400,7 @@ window._MOVIES_DATA = [
     ],
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/lmrulvLbmaejTix1YaMxo1oGhH1.jpg",
     backdrop: "https://image.tmdb.org/t/p/original/4E5RVsSUG1A4Adwbqupu5w6kofZ.jpg",
-    videoUrl: "1081003",
+    videoUrl: "",
     trailerUrl: "",
     overview: "Kara Zor-El, cousin of Superman, is drawn into a quest for vengeance after a violent attack shatters her fragile peace. Joined by an unexpected ally, she sets out across the galaxy, confronting both a ruthless enemy and her own darker instincts along the way.",
     overviewKurdish: "",
