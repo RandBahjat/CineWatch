801972eef24f9468716a2744d82d6d4ddc7cd294
RandiBajat <randibajat@github.com>
2026-09-18 00:19:55 +0300

    Auto-sync updates

--- cinewatch-app/series-data.js
+++ cinewatch-app/series-data.js
@@ -24,7 +24,7 @@ window._SERIES_DATA = [
     videoUrl: '224263',
     trailerUrl: "https://youtu.be/kajCUhg36R0?si=ZEi-UpKGufeODWRH",
     overview: "Set during the pivotal and turbulent summer of 1985 in Hawkins, Indiana, this anthology spinoff dives deeper into the untold encounters, strange local phenomena, and hidden government cover-ups lurking just beyond the main squad's sight. As eerie radio frequencies crackle and shadows stir beneath the town, ordinary residents and unlikely heroes face terrifying supernatural threats bleeding through from the Upside Down.",
-    overviewKurdish: "فرانسیس نیگلی، لێکۆڵەری تایبەتی شارەزا لە شیکاگۆ و هاوکاری پێشووی جاک ڕیچەر لە یەکەی ١١٠ی لێکۆڵینەوەی تایبەتی سوپا، تووشی شۆک دەبێت کاتێک دەزانێت هاوڕێیەکی نزیکی لە ڕووداوێکی گوماناویدا کوژراوە. نیگلی هەموو تواناکانی شەڕکردن و لێکۆڵینەوەی خۆی بەکاردەهێنێت لەپێناو بەدەستهێنانی دادپەروەری، تاوەکو بکوژەکانی هاوڕێکەی بدۆزێتەوە و ڕووبەڕووی ئەو پیلانە گەورەیە ببێتەوە کە ژیانی لە مەترسی ناوە.",
+    overviewKurdish: "لە هاوینی پڕ لە ڕووداوی ساڵی ١٩٨٥ لە شارۆچکەی هۆکینز، ئەم زنجیرە نوێیە ڕووداوە نەبینراوەکان، دیاردە سەیرەکان و نهێنییە شاردراوەکانی حکومەت ئاشکرا دەکات کە لە پەراوێزی چیرۆکە سەرەکییەکەدا ڕوویانداوە. کاتێک شەپۆلە نادیارەکانی ڕادیۆ تێکدەچن و سێبەری ترسناک لە ژێر زەوییەوە سەرهەڵدەدەن، خەڵکی شارۆچکەکە ڕووبەڕووی بوونەوەرە تۆقێنەرەکانی جیهانی ژێرەوە دەبنەوە.",
     director: 'Nick Santora, Nicholas Wootton',
     cast: [
       'Maria Sten, Greyston Holt, Adeline Rudolph, Matthew Del Negro, Damon Herriman, Jasper Jones'
