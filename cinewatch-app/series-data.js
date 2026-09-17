d951fe84cebaefec35d58731e77af5d94b84efc1
RandiBajat <randibajat@github.com>
2026-09-18 01:42:03 +0300

    Auto-sync updates

--- cinewatch-app/series-data.js
+++ cinewatch-app/series-data.js
@@ -20,7 +20,7 @@ window._SERIES_DATA = [
     trailerUrl: "https://youtu.be/qKGgw7Ob5f4",
     overview: "Deep in the heart of a struggling Dixie town, a desperate family man and his reckless brother-in-law rob a local pill mill, unaware that the business is backed by the ruthless New Orleans mafia. When an unhinged mob enforcer arrives in town to exact brutal retribution, a weary, cancer-stricken local sheriff must race against time to maintain peace and prevent a complete bloodbath.",
     overviewKurdish: "لە شارۆچکەیەکی بچووک و هەژارنشین، پیاوێکی خێزاندار لەگەڵ برای ژنەکەی بەهۆی بێپارەییەوە بڕیار دەدەن دەست بەسەر کلینیکێکی فرۆشتنی دەرمانی بێهۆشکەردا بگرن، بەڵام بێ ئاگان لەوەی شوێنەکە سەر بە مافیای بەهێزی نیوئۆرلینزە. کاتێک پیاوکوژێکی دڕندەی مافیا دێتە شارەکە بۆ تۆڵەسەندنەوە، شێریفی نەخۆش و ماندووی شارۆچکەکە هەوڵ دەدات پێش ئەوەی خوێنێکی زۆر بڕژێت دۆخەکە کۆنتڕۆڵ بکات.",
-    director: "Guy Ritchie, Ronan Bennett",
+    director: "Nicholas Maggio",
     cast: [
       "Tom Hardy, Pierce Brosnan, Paddy Considine, Helen Mirren, Joanne Froggatt, Lara Pulver, Anson Boon, Jasmine Jobson"
     ],
