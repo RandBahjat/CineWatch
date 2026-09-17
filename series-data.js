2df95664f3cb486666e367e01480b54c4f0d69cd
RandiBajat <randibajat@github.com>
2026-09-18 00:22:10 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -66,7 +66,7 @@ window._SERIES_DATA = [
           },
           {
             episode: 8,
-            title: 'Touched'
+            title: 'Chapter Eight: The Party'
           },
           
         ]
