0415df47ced298a516733a85e9981621b3870c24
RandiBajat <randibajat@github.com>
2026-09-17 14:05:43 +0300

    Auto-sync updates

--- cinewatch-app/series-data.js
+++ cinewatch-app/series-data.js
@@ -62,7 +62,7 @@ window._SERIES_DATA = [
           },
           {
             episode: 7,
-            title: 'Lebron's No Jordan'
+            title: 'Lebrons No Jordan'
           },
           {
             episode: 8,
