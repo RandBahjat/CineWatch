3ee8dc648583b8a28e323c0972c1dea7cfdcb24e
RandiBajat <randibajat@github.com>
2026-09-17 14:05:02 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -46,7 +46,7 @@ window._SERIES_DATA = [
           },
           {
             episode: 3,
-            title: 'Episode #1.3'
+            title: 'Hammer Time'
           },
           {
             episode: 4,
