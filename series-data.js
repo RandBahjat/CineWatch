39bfb7c26d70147d04c6f111394db05fa9d4060b
RandiBajat <randibajat@github.com>
2026-09-18 00:20:20 +0300

    Auto-sync updates

--- series-data.js
+++ series-data.js
@@ -29,7 +29,7 @@ window._SERIES_DATA = [
     cast: [
       'Joe Keery, Maya Hawke, Priah Ferguson, Brett Gelman, Cary Elwes, Randy Havens'
     ],
-    trending: tren,
+    trending: true,
     featured: false,
     is4k: false,
     seasons: [
