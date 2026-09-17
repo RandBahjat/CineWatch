ff1eef6f95a165751b961e22307afc5ce3944ecb
RandiBajat <randibajat@github.com>
2026-09-18 00:49:48 +0300

    Auto-sync updates

--- movie.js
+++ movie.js
@@ -11,7 +11,7 @@ if (window.location.hash.includes("type=recovery")) {
 // ==========================================
 // 1. HIGHLIGHTS & TRENDING
 // ==========================================
-let FEATURED_TITLES = ["Neagley","Reacher","Resident Evil","The End of Oak Street","Stranger Things: Tales from '85","Slow Horses","City of Blood","Mayday","The Runner","Coyote vs. Acme","City of Blood","Spider-Man: Brand New Day","The Whisper Man",
+let FEATURED_TITLES = ["Neagley","Reacher","Resident Evil","The End of Oak Street","Stranger Things: Tales from '85","Slow Horses","City of Blood","Mayday","The Runner","Coyote vs. Acme","Lanterns","Spider-Man: Brand New Day","The Whisper Man",
   "Drawn Together"];
 let TOP_10_TRENDING_TODAY = ["Neagley",
   "Resident Evil","The End of Oak Street","Reacher","Spider-Man: Brand New Day","Mayday" , "One Night Only","Reacher","Mutiny", "Lanterns"];
