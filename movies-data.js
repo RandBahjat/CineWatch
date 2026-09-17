cb684edee7d5c225b5e64b175019658c4bcf57c0
RandiBajat <randibajat@github.com>
2026-09-18 01:10:57 +0300

    Auto-sync updates

--- movies-data.js
+++ movies-data.js
@@ -22,7 +22,7 @@ window._MOVIES_DATA = [
     trailerUrl: "https://youtu.be/GwSvAyaMGMo?si=lNcOoYpHjvEkgQIc",
     overview: "Set against the deep, secluded backwoods of rural Oklahoma, an enigmatic and solitary man named Amziah King takes a troubled young foster child under his wing. When dangerous outside forces and shadowy figures from his past encroach on their quiet mountain sanctuary, Amziah must rely on his sharp survival instincts and violent past to defend their home at any cost.",
     overviewKurdish: "لە ناو دارستانە دوورەدەست و کێوییەکانی ئۆکلاھۆما، پیاوێکی تەنیا و نامۆ بە ناوی ئەمزیای کینگ منداڵێکی بێکەس دەگرێتە خۆ و فێری ڕێساکانی مانەوەی دەکات. بەڵام کاتێک کەسانی مەترسیدار و سێبەرە تاریکەکانی ڕابردووی دێنەوە سەر ڕێگای و پەلاماری پەناگەکەی دەدەن، ئەمزیای ناچار دەبێت پەنا بۆ توندوتیژی و تواناکانی پێشووی ببات بۆ پاراستنی ژیانی خۆی و ئەو منداڵە.",
-    director: "Scott Waugh",
+    director: "Andrew Patterson",
     cast: ["Alan Ritchson, Owen Wilson, Rodrigo Santoro, Leila George, Adriana Barraza, Peta Sergeant"],
     trending: true,
     featured: true,
