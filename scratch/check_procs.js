803eabef87e65fd9b8af037c41e20e87ee3fc9fe
RandiBajat <randibajat@github.com>
2026-09-21 20:55:55 +0300

    Auto-sync updates

--- scratch/check_procs.js
+++ scratch/check_procs.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 
 try {
-  const output = execSync('wmic process where "name=\'node.exe\'" get ProcessId,CommandLine', { encoding: 'utf8' });
+  const output = execSync('powershell "Get-WmiObject Win32_Process -Filter \\"name = \'node.exe\'\\" | Select-Object ProcessId, CommandLine | Format-List"', { encoding: 'utf8' });
   console.log(output);
 } catch (e) {
   console.error(e);
