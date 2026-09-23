41b04740e90d9e520fce0f1be02877cf0041f3a1
RandiBajat <randibajat@github.com>
2026-09-24 00:20:26 +0300

    Auto-sync updates

--- cinewatch-app/movies-data.js
+++ cinewatch-app/movies-data.js
@@ -45,7 +45,7 @@ window._MOVIES_DATA = [
     trailerUrl: "https://youtu.be/nC0Gu5AXP9M?si=jOEnIIg_UJyOcobR",
     overview: "Digger Rockwell, an eccentric and phenomenally wealthy oil tycoon widely regarded as the most powerful man in the world, unleashes an unprecedented global disaster. In a state of spiraling madness and colossal ego, he embarks on an increasingly erratic, catastrophic mission to prove to the entire planet that he alone is humanity's ultimate savior before the fallout obliterates civilization.",
     overviewKurdish: "دیگەر ڕۆکوێڵ، بازرگانێکی دەوڵەمەندی نەوت و بەهێزترین پیاوی جیهان، بەهۆی بڕیارە کتوپڕەکانیەوە کارەساتێکی گەورەی جیهانی بەرپا دەکات. لە کاتێکدا جیهان بەرەو وێرانی دەچێت، دیگەر دەکەوێتە ناو کێبڕکێیەکی شێتانە لەگەڵ کات تاوەکو بیسەلمێنێت کە تەنها خۆی دەتوانێت مرۆڤایەتی لەو وێرانکارییە ڕزگار بکات کە خۆی بە دەستی خۆی دروستی کردووە.",
-    director: "Claire Scanlon",
+    director: "Alejandro G. Iñárritu",
     cast: ["Lili Reinhart, Tom Bateman, Rachel Marsh, Jaboukie Young-White, Nicholas Duvernay, Arty Froushan"],
     trending: true,
     featured: true,
