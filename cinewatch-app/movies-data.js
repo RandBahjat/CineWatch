d0b8c4021dcd09b69f71ad2aa0db6ab04a23eef9
RandiBajat <randibajat@github.com>
2026-09-23 23:56:59 +0300

    Auto-sync updates

--- cinewatch-app/movies-data.js
+++ cinewatch-app/movies-data.js
@@ -19,7 +19,7 @@ window._MOVIES_DATA = [
     videoUrl: "1607127",
     trailerUrl: "https://youtu.be/MqpsP5zGYtM?si=ljq6MUEqBoVmWOFm",
     overview: "Seasoned Navy SEAL Jake Harris is called back into action when the world is threatened by a disillusioned former comrade turned domestic terrorist, Elliot Carter. With the Joint Chiefs of Staff held hostage in a nuclear bunker and the clock ticking before the codes are extracted, Harris must lead his unit straight into enemy territory to stop the mercenaries from hijacking America's missile defense network in a relentless, continuous-take battle.",
-    overviewKurdish: "دوای کەوتنەخوارەوەی فڕۆکەکەیان لە ناوچە سەهۆڵاوییە چۆڵەوانەکانی ئەلاسکا، جەیمس بێلمۆنت، ئەفسەری پێشووی هێزە تایبەتەکان، لەگەڵ ئۆدینی سەگی سەربازیی خۆیدا لە دۆخێکی سەختدا گیر دەخۆن. بەبێ بوونی هیچ هیوایەکی رزگارکردن، دەبێت ٥٨ میل بەناو بەفر، سەرما و دڕندە کێوییەکاندا ببڕن بۆ مانەوە لە ژیاندا، لە کاتێکدا پەیوەندی و وەفاداریی نێوان ئەم سەربازە و سەگەکەی دەبێتە تاکە هێز بۆ بەرگەگرتنی ئەم جەنگە قورسە.",
+    overviewKurdish: "جەیک هاریست سەربازی بەئەزموونی هێزە تایبەتەکانی دەریایی (Navy SEAL)، ناچار دەبێتەوە بگەڕێتەوە بۆ ناو مەیدانی جەنگ کاتێک هاوڕێ و سەربازی پێشووی، ئیلیۆت کارتەر، دەبێتە تیرۆریستێکی مەترسیدار و دەست بەسەر ژووری فەرماندەیی چەکی ئەتۆمیدا دەگرێت. لە کاتێکدا کات بەرەو تەواوبوون دەچێت و مەترسیی لەکارخستنی سیستەمی بەرگریی مووشەکی دروست دەبێت، هاریست دەبێت بە تەنیا و لە ڕێگەی شەڕێکی بێوەستان و نەپچڕاوەوە بەرەنگاری ئەم پیلانە مەرگهێنەرە ببێتەوە.",
     director: "David Ayer",
     cast: ["Brad Pitt, J.K. Simmons, Anna Lambe, Uber (Odin)"],
     trending: true,
