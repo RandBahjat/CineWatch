12e3f67e53aeb5301034354ce197e70b04b50e06
RandiBajat <randibajat@github.com>
2026-09-25 12:32:46 +0300

    Auto-sync updates

--- cinewatch-app/movie.js
+++ cinewatch-app/movie.js
@@ -4086,7 +4086,7 @@ const VIP_WALLETS = {
   },
   fastpay: {
     name: "FastPay",
-    number: "0750 000 0000",
+    number: "0774 820 1148",
     holder: "CineWatch VIP",
     note: "Send payment via FastPay mobile app to this number.",
     color: "#e11d48",
@@ -4094,7 +4094,7 @@ const VIP_WALLETS = {
   },
   zaincash: {
     name: "ZainCash",
-    number: "0780 000 0000",
+    number: "0774 820 1148",
     holder: "CineWatch VIP",
     note: "Send cash transfer via ZainCash wallet to this phone number.",
     color: "#007a78",
@@ -4303,7 +4303,7 @@ function selectVipTier(tierData) {
   if (tgBtn) tgBtn.href = `https://t.me/randibajat?text=${msg}`;
 
   const waBtn = document.getElementById("vipWhatsappBtn");
-  if (waBtn) waBtn.href = `https://wa.me/?text=${msg}`;
+  if (waBtn) waBtn.href = `https://wa.me/9647748201148?text=${msg}`;
 
   renderVipWalletDetails(currentVipWalletKey);
 }
